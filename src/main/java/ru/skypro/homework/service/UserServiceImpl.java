89f5b4c56d428996b0f3e10fdb199d5830239bba
KrisEzh <91995429+KrisEzh@users.noreply.github.com>
2022-07-28 23:39:01 +0300

    Сделала UserController

---
+++ src/main/java/ru/skypro/homework/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package ru.skypro.homework.service;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.service.impl.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
