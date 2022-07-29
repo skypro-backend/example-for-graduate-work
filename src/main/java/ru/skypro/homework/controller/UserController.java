59b826e7d30e73ff294551d9d7ca9d4a99e02561
Aleksey Pakhomov <Alexey007-89@mail.ru>
2022-07-29 16:42:23 +1000

    Добавил в UserController ResponseWrapperUser

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -2,7 +2,9 @@ package ru.skypro.homework.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.CreateUser;
 import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.ResponseWrapperUser;
 import ru.skypro.homework.dto.User;
 
 import java.util.Collection;
@@ -12,23 +14,23 @@ import java.util.Collection;
 @RequestMapping("/users")
 public class UserController {
     @PostMapping
-    public ResponseEntity<User>addUser(@RequestBody User user){
-        return ResponseEntity.ok(user);
+    public ResponseEntity<CreateUser>addUser(@RequestBody CreateUser createUser){
+        return ResponseEntity.ok(createUser);
     }
     @PatchMapping("/me")
     public ResponseEntity<User>updateUser(@RequestBody User user){
         return ResponseEntity.ok(user);
     }
     @GetMapping("/me")
-    public ResponseEntity<Collection<User>> getUsers(){
-        return ResponseEntity.ok(null);
+    public ResponseEntity<ResponseWrapperUser> getUsers(){
+        return ResponseEntity.ok(new ResponseWrapperUser());
     }
     @PostMapping("/set_password")
     public ResponseEntity<NewPassword>setPassword(@RequestBody NewPassword newPassword) {
         return ResponseEntity.ok(newPassword);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<User>getUser(@PathVariable("id") int id){
+    public ResponseEntity<User>getUser(@PathVariable int id){
         return ResponseEntity.ok(new User());
     }
 }
