9fbe0ca83125717262ad2af4e57f7d7963e36bfc
EDyugaeva <ed2408@yandex.ru>
2022-09-19 21:32:24 +0300

    cherrypick

---
+++ src/main/java/ru/skypro/homework/models/exceptions/ErrorResponse.java
@@ -0,0 +1,20 @@
+package ru.skypro.homework.models.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    private String message;
+    private HttpStatus httpStatus;
+
+
+}
