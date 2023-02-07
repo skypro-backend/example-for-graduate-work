d56cd312298c3f0d9178e94c3506381a0098af72
igr76 <gritsuk76@mail.ru>
2023-02-08 08:44:19 +1000

    создание классов контроллера

---
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@AllArgsConstructor
+@RestController
+public class AdsController {
+}
