b5639fe2b9276978604d0d91f5f2e51dd4329c2a
Sergej <lupaevsv@gmail.com>
2023-02-09 02:02:42 +0700

    update

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,6 +1,12 @@
 package ru.skypro.homework.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -25,20 +31,31 @@ public class AdsController {
         this.adsService = adsService;
     }
 
+    @Operation(summary = "Получение всех комментариев у объявления")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found All comments",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Comment.class)))
+                    })
+    })
     @GetMapping("/{ad_pk}/comment")
     public ResponseEntity<Collection<Comment>> getAdsComments(@PathVariable(name = "ad_pk") @NonNull Integer pk) {
         return ResponseEntity.ok().body(adsService.getAdsComments(pk));
     }
 
     @PostMapping("/{ad_pk}/comment")
-    public ResponseEntity<?> addAdsComments (@PathVariable(name = "ad_pk") @NonNull Integer pk) {
+    public ResponseEntity<?> addAdsComments(@PathVariable(name = "ad_pk") @NonNull Integer pk) {
         adsService.addAdsComments(pk);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{ad_pk}/comment/{id}")
-    public ResponseEntity<?> deleteAdsComment (@PathVariable(name = "ad_pk") @NonNull Integer pk,
-                                               @PathVariable(name = "id") @NonNull Integer id) {
+    public ResponseEntity<?> deleteAdsComment(@PathVariable(name = "ad_pk") @NonNull Integer pk,
+                                              @PathVariable(name = "id") @NonNull Integer id) {
         adsService.deleteAdsComment(pk, id);
         return ResponseEntity.ok().build();
     }
