34a6485a91ab395a038f8712a001c6a22758a3c5
Sergej <lupaevsv@gmail.com>
2023-02-08 23:56:33 +0700

    update

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,5 +1,13 @@
 package ru.skypro.homework.controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ads")
+@CrossOrigin(value = "http://localhost:3000")
 public class AdsController {
 
 }
--- src/main/java/ru/skypro/homework/dto/Comment.java
+++ src/main/java/ru/skypro/homework/dto/Comment.java
@@ -1,7 +1,11 @@
 package ru.skypro.homework.dto;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+
+@Data
 public class Comment {
 
     /**
