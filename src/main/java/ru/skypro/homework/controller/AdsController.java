34f4c997228e005203c746546e24fff22e4cc8ec
AntonGraf <ant0ngraf@yandex.ru>
2023-02-10 14:42:27 +0600

    Merge commit

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -9,32 +9,18 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Collection;
-import java.util.Map;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.homework.dto.Comment;
-import ru.skypro.homework.record.AdRecord;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.service.AdsService;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
 import java.util.Map;
-
 
 @RestController
 @RequestMapping("/ads")
@@ -299,12 +285,12 @@ public class AdsController {
   @Min(value = 1, message = "Идентификатор должен быть больше 0")
   @Parameter(description = "Идентификатор объявления",
       example = "1") String adPk,
-      @PathVariable(name = "id")
+                                                   @PathVariable(name = "id")
       @NotBlank(message = "id не должен быть пустым")
       @Min(value = 1, message = "Идентификатор должен быть больше 0")
       @Parameter(description = "Идентификатор комментария",
           example = "1") int id,
-      @RequestBody CommentDTO commentDTO) {
+                                                   @RequestBody CommentDTO commentDTO) {
     return ResponseEntity.ok(adsService.updateComments(adPk, id, commentDTO));
   }
 
