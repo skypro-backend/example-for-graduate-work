483d0ae128dd842074f18b7f349b6a8d2b684013
sergej <lupaevsv@gmail.com>
2023-02-13 11:08:04 +0700

    Update

--- src/main/java/ru/skypro/homework/repository/ImageRepository.java
+++ src/main/java/ru/skypro/homework/repository/ImageRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.ImageEntity;
 
-@Repository
-public interface ImageRepository  extends JpaRepository<ImageEntity, Long> {
-
-}
+//@Repository
+//public interface ImageRepository  extends JpaRepository<ImageEntity, Long> {
+//
+//}
