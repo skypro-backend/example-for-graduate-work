d831641dca9626bb13a0fc74ce2704b631bba0aa
AntonGraf <ant0ngraf@yandex.ru>
2023-02-13 08:21:52 +0600

    Подключает БД PostgreSQL, liquibase, создает таблицу Users

---
+++ src/main/resources/db.changelog/v-1.0/01-create-table-users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users
+(
+    id  serial PRIMARY KEY ,
+    email TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    password TEXT,
+    phone TEXT,
+    reg_date TIMESTAMP,
+    city TEXT,
+    image BYTEA
+);
