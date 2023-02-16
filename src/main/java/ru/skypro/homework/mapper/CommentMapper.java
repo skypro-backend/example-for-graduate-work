25573bfeb07619c1a86c58bc69db65266826fb7d
AntonGraf <ant0ngraf@yandex.ru>
2023-02-16 15:25:18 +0600

    Реализовывает doDTO в CommentMapper и тест для проверки

--- src/main/java/ru/skypro/homework/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -1,10 +1,9 @@
 package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.entity.CommentEntity;
-
-import java.util.Collection;
 
 /**
  * маппер для {@link CommentEntity}
@@ -12,12 +11,15 @@ import java.util.Collection;
  */
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
-    CommentEntity toEntity(CommentDTO commentDTO);
+//    CommentEntity toEntity(CommentDTO commentDTO);
 
+    @Mapping(target = "createdAt", source = "createdAt", dateFormat = "dd-MM-yyyy HH:mm:ss")
+    @Mapping(target = "author", source = "author.id")
+    @Mapping(target = "pk", source = "pk.pk")
     CommentDTO toDTO(CommentEntity commentEntity);
 
-    Collection<CommentEntity> toEntityList(Collection<CommentDTO> CommentDTOS);
+//    Collection<CommentEntity> toEntityList(Collection<CommentDTO> CommentDTOS);
 
-    Collection<CommentDTO> toDTOList(Collection<CommentEntity> CommentEntities);
+//    Collection<CommentDTO> toDTOList(Collection<CommentEntity> CommentEntities);
 
 }
