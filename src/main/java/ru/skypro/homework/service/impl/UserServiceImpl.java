11fa8ed14e303538046de1ce98920d48ef792fda
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-02-20 14:05:49 +0600

    иисправил обновление фото updateUserImage

--- src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -33,8 +34,8 @@ public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
   private final UserMapper userMapper;
 
-  @Value("./user_photo")
-  private String userPhotoDit;
+  @Value("${image.user.dir.path}")
+  private String userPhotoDir;
 
   public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
     this.userRepository = userRepository;
@@ -90,23 +91,23 @@ public class UserServiceImpl implements UserService {
     String nameEmail = authentication.getName();
     UserEntity userEntity = findEntityByEmail(nameEmail);
 
-    Path filePath = Path.of("");
+    Path filePath = Path.of(userPhotoDir, Objects.requireNonNull(image.getOriginalFilename()));
+    try {
+      Files.createDirectories(filePath.getParent());
+      Files.deleteIfExists(filePath);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
 
     try (InputStream is = image.getInputStream();
         OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
         BufferedInputStream bis = new BufferedInputStream(is, 1024);
         BufferedOutputStream bos = new BufferedOutputStream(os, 1024)) {
 
-      long id = userEntity.getId();
-      filePath = Path.of(userPhotoDit, id + "." + getExtension(
-          Objects.requireNonNull(image.getOriginalFilename())));
-      Files.createDirectories(filePath.getParent());
-      Files.deleteIfExists(filePath);
-
       bis.transferTo(bos);
 
       String photo = Base64.getEncoder().encodeToString(image.getBytes());
-      userEntity.setPhone(photo);
+      userEntity.setImage(photo);
       userRepository.save(userEntity);
 
     } catch (Exception e) {
@@ -137,15 +138,5 @@ public class UserServiceImpl implements UserService {
     return userRepository.findByEmail(email);
   }
 
-  /**
-   * вспомогательный медот для загрузки фотографий
-   *
-   * @return расширение файла
-   */
-  String getExtension(String fileName) {
-    log.info(FormLogInfo.getInfo());
-
-    return fileName.substring(fileName.lastIndexOf(".") + 1);
-  }
 
 }
