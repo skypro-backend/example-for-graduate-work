4700168667bc671c2488a31b00bcf0a62fc17e53
sergej <lupaevsv@gmail.com>
2023-02-23 11:31:44 +0700

    ImageControllerTest

---
+++ src/test/java/ru/skypro/homework/WebSecurityConfigTest.java
@@ -0,0 +1,53 @@
+package ru.skypro.homework;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class WebSecurityConfigTest {
+
+  private static final String[] AUTH_WHITELIST = {
+      "/swagger-resources/**",
+      "/swagger-ui.html",
+      "/v3/api-docs",
+      "/webjars/**",
+      "/login", "/register"
+
+  };
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user@gmail.com")
+        .password("password")
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf().disable()
+        .authorizeHttpRequests((authz) ->
+            authz
+                .mvcMatchers(AUTH_WHITELIST).permitAll()
+                .mvcMatchers("/ads/**", "/users/**").authenticated()
+
+        )
+        .cors().disable()
+        .httpBasic(withDefaults());
+    return http.build();
+  }
+
+
+}
+
