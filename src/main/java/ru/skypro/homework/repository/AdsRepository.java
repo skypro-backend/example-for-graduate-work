7a443e0976ac97e1081fd3bedbdccade7a8b7951
dmitr <dmitriypospelov93@gmail.com>
2023-02-24 14:28:15 +0700

    Like methods in repo

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -1,8 +1,8 @@
 package ru.skypro.homework.repository;
 
+import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.entity.AdEntity;
 
 /**
@@ -12,6 +12,8 @@ import ru.skypro.homework.entity.AdEntity;
 @Repository
 public interface AdsRepository extends JpaRepository<AdEntity, Integer> {
     AdEntity findByAuthorAndId(int author,int id);
+
+    Collection<AdEntity> findAdEntityByTitleContaining(String title);
 
 
 }
