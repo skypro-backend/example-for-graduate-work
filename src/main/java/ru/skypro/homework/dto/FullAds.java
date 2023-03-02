87a31365f75eecd9b1fc52cce15db3e3639a8016
AnikeevIgor <415b@mail.ru>
2023-03-02 20:52:29 +0700

    feature addDTO

---
+++ src/main/java/ru/skypro/homework/dto/FullAds.java
@@ -0,0 +1,16 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class FullAds {
+    private String image;
+    private String authorLastName;
+    private String authorFirstName;
+    private String phone;
+    private String description;
+    private String title;
+    private String email;
+    private Integer price;
+    private Integer id; //pk
+}
