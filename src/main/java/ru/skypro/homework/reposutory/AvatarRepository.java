19f54f0e466738ef4e6cb67b4047849090b0267e
Martell <vovasamb@yandex.ru>
2023-03-07 19:46:41 +0300

    Revert "Часть классов для Avatar и Poster"
    This reverts commit a9cde11964742c6a8896dc466d9425717778ea13.

--- src/main/java/ru/skypro/homework/reposutory/AvatarRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.skypro.homework.reposutory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.entity.AvatarEntity;
-
-public interface AvatarRepository extends JpaRepository<AvatarEntity, Long> {
-    AvatarEntity findByUserId(Long userId);
-}
