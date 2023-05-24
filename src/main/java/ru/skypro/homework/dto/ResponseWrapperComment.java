938dc9d71c46f95f822ae2b0e7fc8679b5043a6f
Alexander <emotionless013@mail.ru>
2023-05-24 20:49:27 +0400

    создал DTO

---
+++ src/main/java/ru/skypro/homework/dto/ResponseWrapperComment.java
@@ -0,0 +1,10 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class ResponseWrapperComment {
+    private int count;
+    private List<Comment> results;
+}
