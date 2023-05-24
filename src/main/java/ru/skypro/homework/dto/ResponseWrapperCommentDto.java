3f3761051212fafaeefb2b3317298e720f0ed0e2
Alexander <emotionless013@mail.ru>
2023-05-24 22:24:56 +0400

    создал DTO

--- src/main/java/ru/skypro/homework/dto/ResponseWrapperCommentDto.java
+++ src/main/java/ru/skypro/homework/dto/ResponseWrapperCommentDto.java
@@ -5,6 +5,6 @@ import lombok.Data;
 import java.util.List;
 @Data
 public class ResponseWrapperCommentDto {
-     private int count;
+    private int count;
     private List<CommentDto> results;
 }
