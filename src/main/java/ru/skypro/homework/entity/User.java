b69a56681c8f3bb2db9c104597213bd17f167f68
olesiapetrova <olexandro@gmail.com>
2023-07-03 22:28:13 +0300

    Добавлены необходимые зависимости

--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import ru.skypro.homework.dto.Role;
 
 import javax.persistence.*;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
