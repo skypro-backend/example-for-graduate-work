e1851d6eaa006785c25c2f83efb4a221d8b64e8f
Айрат <airat_vip@mail.ru>
2023-07-12 21:45:07 +0300

    создал модели

---
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -0,0 +1,54 @@
+package ru.skypro.homework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.skypro.homework.dto.Role;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    private List<Ad> ad = new ArrayList<>();
+
+    @OneToMany()
+    @JoinColumn(name = "user_id")
+    private List<Comment> comments = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name = "image")
+    private String image;
+
+}
