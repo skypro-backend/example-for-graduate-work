eb2c1b77c541cbabe3d96796fdc8c296c94aaedc
Vik163tlt <@Vik163tlt@ya.ru>
2023-07-20 23:58:00 +0300

    + AdsImageRepository and UserImageRepository

--- src/main/java/ru/skypro/homework/repository/UserImageRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserImageRepository.java
@@ -2,10 +2,8 @@ package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.skypro.homework.model.Comment;
-
+import ru.skypro.homework.model.UserImage;
 
 @Repository
-public interface CommentsRepository extends JpaRepository<Comment, Integer> {
-
+public interface UserImageRepository extends JpaRepository<UserImage, Integer> {
 }
--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.model.User;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
