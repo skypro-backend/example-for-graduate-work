f7f3552f8ef0bfc2e99b7c69d7c2b462485cf54e
Айрат <airat_vip@mail.ru>
2023-07-24 16:16:36 +0300

    поправил getUser

--- src/main/java/ru/skypro/homework/service/impl/UserService.java
+++ src/main/java/ru/skypro/homework/service/impl/UserService.java
@@ -1,11 +1,6 @@
 package ru.skypro.homework.service.impl;
 
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,31 +11,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import ru.skypro.homework.dto.NewPasswordDto;
 import ru.skypro.homework.dto.UpdateUserDto;
-import ru.skypro.homework.dto.UserDetailsDto;
 import ru.skypro.homework.model.User;
-import ru.skypro.homework.model.UserPrincipal;
 import ru.skypro.homework.repository.UserRepository;
 
+import java.util.Optional;
+
 @Service
-@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final ImageService imageService;
 
     private final UserRepository userRepository;
-    private final PasswordEncoder encoder;
+
+    public UserService(ImageService imageService, UserRepository userRepository) {
+        this.imageService = imageService;
+        this.userRepository = userRepository;
+    }
 
     public User getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String name = authentication.getName();
-        System.out.println(name);
-        return userRepository.findUserByUsername(name).orElseThrow();
+        return userRepository.findUserByLogin(authentication.getName()).orElseThrow();
     }
 
 
     public void updatePassword(NewPasswordDto newPasswordDto) {
         User user = getUser();
-        user.setPassword(encoder.encode(newPasswordDto.getNewPassword()));
+        user.setPassword(newPasswordDto.getNewPassword());
         userRepository.save(user);
     }
 
@@ -60,13 +56,21 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findUserByUsername(username).orElseThrow();
-        UserDetailsDto userDetailsDto = new UserDetailsDto(user.getUsername(), user.getPassword(), user.getId(), user.getRole());
-        return new UserPrincipal(userDetailsDto);
+        User user = userRepository.findUserByLogin(username).orElseThrow();
+        if (user == null) {
+            throw new UsernameNotFoundException("Unknown user" + username);
+        }
+        UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+                .username(user.getLogin())
+                .password(user.getPassword())
+                .roles(String.valueOf(user.getRoleDto()))
+                .build();
+
+        return userDetails;
     }
 
-        public boolean userExists (String username ){
-            userRepository.findUserByUsername(username).isPresent();
-            return true;
-        }
+    public boolean userExists(String username) {
+        userRepository.findUserByLogin(username).isPresent();
+        return true;
     }
+}
