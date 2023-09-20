497c7fa1ed0eb70cc5cbab8d0145e5cee6ff5a6a
Lelena1 <luppol@inbox.ru>
2023-09-21 00:35:51 +0300

    Initial commit

---
+++ src/test/java/ru/skypro/homework/mappers/AdMapperTest.java
@@ -0,0 +1,28 @@
+package ru.skypro.homework.mappers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdMapperTest {
+
+    @Test
+    void toAd() {
+    }
+
+    @Test
+    void toAdDto() {
+    }
+
+    @Test
+    void toExtendedAdDto() {
+    }
+
+    @Test
+    void toAdsDto() {
+    }
+
+    @Test
+    void updateAds() {
+    }
+}
