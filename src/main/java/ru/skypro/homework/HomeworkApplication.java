20bc4725452993881ad913a96d1d9717cc10bff9
Drogolova <drogolova1997@mail.ru>
2023-09-24 23:35:47 +0300

    add docker-compose.yml

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration (exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration (exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@EnableJpaRepositories
 public class HomeworkApplication {
   public static void main(String[] args) {
     SpringApplication.run(HomeworkApplication.class, args);
