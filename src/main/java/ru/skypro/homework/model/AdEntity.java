1fcc93491f9ef5b74c1fd954c78f6e0ecb49fca6
porovoz <falkon1@list.ru>
2023-09-25 21:49:39 +0300

    AdEntity and CommentEntity classes created.

---
+++ src/main/java/ru/skypro/homework/model/AdEntity.java
@@ -0,0 +1,57 @@
+package ru.skypro.homework.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "ads")
+public class AdEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "image_path")
+    private String imagePath;
+
+    @Column(name = "image_media_type")
+    private String imageMediaType;
+
+    @Column(name = "image_file_size")
+    private long imageFileSize;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ToString.Exclude
+    private UserEntity userEntity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AdEntity adEntity = (AdEntity) o;
+        return id == adEntity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
