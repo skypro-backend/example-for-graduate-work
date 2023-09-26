35fe72d5594f1037525c254f3901b614a214aa71
ShevoFirst <play.job@yandex.ru>
2023-09-26 22:43:03 +0400

    add AdMapper

---
+++ src/main/java/ru/skypro/homework/store/mappers/AdMapper.java
@@ -0,0 +1,12 @@
+package ru.skypro.homework.store.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import ru.skypro.homework.dto.AdDTO;
+import ru.skypro.homework.store.entities.AdEntity;
+@Mapper
+public interface AdMapper {
+    AdMapper INSTANCE = Mappers.getMapper( AdMapper.class );
+
+    AdDTO toDto(AdEntity ad);
+}
