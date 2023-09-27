b5f030b075d38e466f86f81cc5badb2afc40fe85
destmecture <temzzsv@mail.ru>
2023-09-27 15:00:57 +0300

    Refactor authController, add swagger annotation, add validation annotation for request bodies

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -1,5 +1,9 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,6 +16,9 @@ import ru.skypro.homework.dto.authentication.LoginDto;
 import ru.skypro.homework.dto.authentication.RegisterDto;
 import ru.skypro.homework.service.AuthService;
 
+import javax.persistence.TableGenerator;
+import javax.validation.Valid;
+
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
@@ -20,8 +27,14 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Tag(name = "Авторизация")
+    @Operation(summary = "Авторизация пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<?> login(@RequestBody @Valid LoginDto loginDto) {
         if (authService.login(loginDto.getUsername(), loginDto.getPassword())) {
             return ResponseEntity.ok().build();
         } else {
@@ -29,8 +42,14 @@ public class AuthController {
         }
     }
 
+    @Tag(name = "Регистрация")
+    @Operation(summary = "Регистрация пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request")
+    })
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterDto registerDto) {
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterDto registerDto) {
         if (authService.register(registerDto)) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } else {
