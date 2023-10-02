e4f844ccca8c48cf36da0a1a8f0868195ec3efd9
Svetlana4278Zh <svetlana.zh@inbox.ru>
2023-10-03 01:02:20 +0300

    corrections

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.skypro.homework.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -47,13 +48,21 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
+                .formLogin(formLogin -> formLogin.loginProcessingUrl("/login"))
+                .logout(logout -> logout.logoutUrl("logout"))
                 .authorizeHttpRequests(
                         authorization ->
                                 authorization
                                         .mvcMatchers(AUTH_WHITELIST)
                                         .permitAll()
+                                        .mvcMatchers(HttpMethod.GET,"/ads")
+                                        .permitAll()
+                                        .mvcMatchers(HttpMethod.DELETE,"ads/{id}", "ads/{adId}/comments/{commentId}")
+                                        .hasRole("ADMIN")
+                                        .mvcMatchers(HttpMethod.PATCH,"ads/{id}", "ads/{adId}/comments/{commentId}")
+                                        .hasRole("ADMIN")
                                         .mvcMatchers("/ads/**", "/users/**")
-                                        .authenticated())
+                                        .hasRole("USER"))
                 .cors()
                 .and()
                 .httpBasic(withDefaults());
