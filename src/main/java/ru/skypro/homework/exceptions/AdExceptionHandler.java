ece048c3d5944e39c6caf668d12f746e40782623
katiavasileva <eka.vasileva@gmail.com>
2023-10-02 22:35:17 +0500

    Создала ExceptionHandler + CommentNotFoundException

---
+++ src/main/java/ru/skypro/homework/exceptions/AdExceptionHandler.java
@@ -0,0 +1,54 @@
+package ru.skypro.homework.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class AdExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleIOException(IOException ioException) {
+        return new ResponseEntity<>(ioException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleException(Exception exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleNotFoundException(AdNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleNotFoundException(CommentNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleNotFoundException(FileNotFoundException exception) {
+        return new ResponseEntity<>("Файл не найден", HttpStatus.NOT_FOUND);
+    }
+
+}
