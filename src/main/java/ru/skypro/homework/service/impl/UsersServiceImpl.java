317d7525c269ab2ef0b39e1c4a3695972266f0b5
katiavasileva <eka.vasileva@gmail.com>
2023-10-05 12:03:11 +0500

    Исправления в Users

--- src/main/java/ru/skypro/homework/service/impl/UsersServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UsersServiceImpl.java
@@ -2,10 +2,7 @@ package ru.skypro.homework.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,32 +22,31 @@ public class UsersServiceImpl implements UsersService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
     private final UserMapper userMapper;
 
     @Override
     @Transactional
-    public void setPassword(String currentPassword, String newPassword) {
-        UserEntity userEntity = getCurrentUser();
+    public void setPassword(String currentPassword, String newPassword, String username) {
+        UserEntity userEntity = userRepository.findByUsername(username);
         if (passwordEncoder.matches(currentPassword, userEntity.getPassword())) {
             userEntity.setPassword(passwordEncoder.encode(newPassword));
             userRepository.save(userEntity);
         } else {
-            throw new BadCredentialsException("Пароль не соответствует текущему");
+            throw new BadCredentialsException("Неверный текущий пароль");
         }
     }
 
     @Override
     @Transactional
-    public User getUser() {
-        UserEntity userEntity = getCurrentUser();
+    public User getUser(String username) {
+        UserEntity userEntity = userRepository.findByUsername(username);
         return userMapper.userEntityToUser(userEntity);
     }
 
     @Override
     @Transactional
-    public UpdateUser updateUser(UpdateUser updateUser) {
-        UserEntity userEntity = getCurrentUser();
+    public UpdateUser updateUser(UpdateUser updateUser, String username) {
+        UserEntity userEntity = userRepository.findByUsername(username);
         userEntity.setFirstName(updateUser.getFirstName());
         userEntity.setLastName(updateUser.getLastName());
         userEntity.setPhone(updateUser.getPhone());
@@ -60,8 +56,8 @@ public class UsersServiceImpl implements UsersService {
 
     @Override
     @Transactional
-    public void updateUserImage(MultipartFile file) {
-        UserEntity userEntity = getCurrentUser();
+    public void updateUserImage(MultipartFile file, String username) {
+        UserEntity userEntity = userRepository.findByUsername(username);
         String filePath = "";
         userEntity.setImage(filePath);
         userRepository.save(userEntity);
@@ -79,11 +75,5 @@ public class UsersServiceImpl implements UsersService {
     @Transactional
     public boolean userExists(String username) {
         return userRepository.findByUsername(username) != null;
-    }
-
-    public UserEntity getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        MyDatabaseUserDetails myDatabaseUserDetails = (MyDatabaseUserDetails) userDetailsService.loadUserByUsername(authentication.getName());
-        return myDatabaseUserDetails.toUserEntity();
     }
 }
--- src/main/java/ru/skypro/homework/utilities/Utils.java
+++ src/main/java/ru/skypro/homework/utilities/Utils.java
@@ -12,11 +12,10 @@ public class Utils {
         long timeDifference = 0;
         SimpleDateFormat obj = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
         try {
-            Date initial = obj.parse("01-01-1970 05:00:00");
+            Date initial = obj.parse("01-01-1970 03:00:00");
             LocalDateTime time = LocalDateTime.now();
             Date now = Timestamp.valueOf(time);
             timeDifference = now.getTime() - initial.getTime();
-
         } catch (ParseException e) {
             e.printStackTrace();
         }
