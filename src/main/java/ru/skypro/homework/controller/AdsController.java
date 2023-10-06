06f33b0e3fc9e6f73b1cd9ffc0cb6f4f2cebe557
Kinserega <kindratsergej5@gmail.com>
2023-10-06 14:03:00 +0300

    Добавил методы Добавления, удаления, получение всех, получение по id объявлений

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -2,6 +2,7 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,23 +10,25 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.Ad;
 import ru.skypro.homework.dto.Ads;
 import ru.skypro.homework.dto.CreateOrUpdateAd;
+import ru.skypro.homework.dto.ExtendedAd;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("ads")
+@RequestMapping("/ads")
 public class AdsController {
 
     /**
      * Метод обновляет информацию об объявлении.
-     * @param id порядковый номер объявления в БД.
+     *
+     * @param id               порядковый номер объявления в БД.
      * @param createOrUpdateAd DTO. Включает title, price и description объявления.
      * @return DTO Ad. Включает author, image, pk, price и title объявления.
      */
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Ad> updateAds(@PathVariable Integer id,
-                                       @RequestBody CreateOrUpdateAd createOrUpdateAd) {
+                                        @RequestBody CreateOrUpdateAd createOrUpdateAd) {
         return ResponseEntity.ok(new Ad());
     }
 
@@ -39,7 +42,8 @@ public class AdsController {
 
     /**
      * Метод обновляет картинку объявления.
-     * @param id порядковый номер объявления в БД.
+     *
+     * @param id        порядковый номер объявления в БД.
      * @param imageFile принимает изображение.
      * @return бинарный код изображения.
      */
@@ -47,5 +51,47 @@ public class AdsController {
     public ResponseEntity<String> updateImage(@PathVariable Integer id,
                                               @RequestParam MultipartFile imageFile) {
         return ResponseEntity.ok("Uploaded");
+    }
+
+    /**
+     * Метод получения всех объявлений
+     * @return возвращает ResponsEntity
+     */
+    @GetMapping
+    public ResponseEntity<Ads> getAllAds() {
+        return ResponseEntity.ok(new Ads());
+    }
+
+    /**
+     * Метод добавления нового объявления
+     * @param createOrUpdateAd
+     * @param imageFile              принимает изображение.
+     * @return возвращает ResponsEntity
+     */
+    // postman не отрабатывает
+    @PostMapping
+    public ResponseEntity<Ad> addAds(@RequestBody CreateOrUpdateAd createOrUpdateAd,
+                                     @RequestParam MultipartFile imageFile) {
+        return ResponseEntity.ok(new Ad());
+    }
+
+    /**
+     * метод удаления объявления по id
+     * @param id            идентификатор объявления
+     * @return возвращает ResponsEntity
+     */
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteAds(@PathVariable Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    /**
+     * метод получения информации объявления по id
+     * @param id идентификатор объявления
+     * @return возвращает ResponsEntity
+     */
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ExtendedAd> getInfoAdsById(@PathVariable Integer id) {
+        return ResponseEntity.ok(new ExtendedAd());
     }
 }
