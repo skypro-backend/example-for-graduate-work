cb44b5031b52723cacb82471d641708e5b362203
porovoz <74820096+porovoz@users.noreply.github.com>
2023-10-07 16:43:36 +0300

    Update src/main/java/ru/skypro/homework/controller/CommentController.java
    Co-authored-by: Dmitry Bizin <bizin.mitya@mail.ru>

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -20,7 +20,7 @@ import ru.skypro.homework.service.CommentService;
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/comments")
+@RequestMapping("/ads")
 @Tag(name = "Комментарии")
 public class CommentController {
 
