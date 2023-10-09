7aee4eae063afb0bd5ddc1b97bc631c03fb64a15
200 <evtepo@gmail.com>
2023-10-09 12:17:25 +0300

    create method in UserService

--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -1,11 +1,15 @@
 package ru.skypro.homework.service;
 
+import ru.skypro.homework.dto.NewPasswordDTO;
 import ru.skypro.homework.dto.UpdateUserDTO;
 import ru.skypro.homework.dto.UserDTO;
 import ru.skypro.homework.service.entities.UserEntity;
 
 public interface UserService {
-    UpdateUserDTO updateUser(UserEntity userEntity);
+    UpdateUserDTO updateUser(UpdateUserDTO updateUserDTO);
 
-    UserDTO getUser(UserEntity userEntity);
+    UserDTO getUser();
+
+    void updatePassword(NewPasswordDTO newPasswordDTO);
 }
+
