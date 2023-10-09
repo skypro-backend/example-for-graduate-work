356c1db5d67e640e6855786020dc8aa677800d84
porovoz <falkon1@list.ru>
2023-10-09 23:06:40 +0300

    downloadAvatarFromDB method added. Created two constants in AdServiceImpl.

--- src/main/java/ru/skypro/homework/service/AccountService.java
+++ src/main/java/ru/skypro/homework/service/AccountService.java
@@ -4,6 +4,7 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.account.NewPassword;
 import ru.skypro.homework.dto.account.User;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface AccountService {
@@ -11,4 +12,5 @@ public interface AccountService {
     User getInfoAboutUser();
     User patchInfoAboutUser(User user);
     boolean updateUserAvatar(MultipartFile image) throws IOException;
+    boolean downloadAvatarFromDB(int userId, HttpServletResponse response) throws IOException;
 }
