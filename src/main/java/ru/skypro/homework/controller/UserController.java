0c0e7354a1e391f4c822eb593e19663c16c14343
AlexBoko <durgen2014@gmail.com>
2023-10-10 15:25:17 +0300

    Этап1

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,44 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterDto> registerUser(@RequestBody RegisterDto registerDto) {
+        return ResponseEntity.ok(new RegisterDto());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginDto> loginUser(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(new LoginDto());
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getUserInfo() {
+        return ResponseEntity.ok(new UserDto());
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<UpdateUserDto> updateUserInfo(@RequestBody UpdateUserDto updateUserDto) {
+        return ResponseEntity.ok(new UpdateUserDto());
+    }
+    @PatchMapping("/set_password")
+    public ResponseEntity<Void> setPassword(@RequestBody NewPasswordDto newPasswordDto) {
+        // Здесь можно реализовать логику обновления пароля.
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/me/image")
+    public ResponseEntity<Void> updateUserImage(@RequestPart MultipartFile image) {
+        // Здесь можно реализовать логику обновления аватара пользователя.
+        return ResponseEntity.ok().build();
+    }
+
+
+}
