2f2a3da0747ce745b213e595b8e63dbc28ac0440
Liz <eliza.an.popova@gmail.com>
2023-10-10 19:17:33 +0300

    Сделаны контроллеры и дто

---
+++ src/main/java/ru/skypro/homework/dto/AdsDto.java
@@ -0,0 +1,17 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+public class AdsDto {
+    private int count;
+    private List<AdDto> results;
+
+    public AdsDto(List<AdDto> list) {
+        count = list == null ? 0 : list.size();
+        results = list;
+    }
+}
