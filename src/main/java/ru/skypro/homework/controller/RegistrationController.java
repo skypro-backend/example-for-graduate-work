8a8c09fb9ced3195ab06d995d5439f24366d95d0
ПК <ekrapostin@gmail.com>
2023-10-11 16:34:36 +0300

    Создал контроллеры, реализовал эндпоинты

---
+++ src/main/java/ru/skypro/homework/controller/RegistrationController.java
@@ -0,0 +1,15 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @PostMapping
+    public void registerUser() {
+
+    }
+}
--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,7 +1,29 @@
 package ru.skypro.homework.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Register;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController {
+
+
+    @PostMapping("/setPassword/")
+    public void updatePassword() {
+
+    }
+
+    @GetMapping("/me/")
+    public Register getInfoAboutUser() {
+        return new Register(); //изменить на DTO
+    }
+
+    @PatchMapping("/me/")
+    public Register updateInfoAboutUser() {
+        return new Register(); //изменить на DTO
+    }
+
+    @PatchMapping("/me/image")
+    public void updateUsersProfilePicture() {
+    }
 }
