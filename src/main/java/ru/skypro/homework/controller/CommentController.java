a101461fdaf875c277d1fcf6ee428bb34ac84ba4
https://github.com/NataDon17/Course2-Homework-11.git <don-nata@mail.ru>
2023-10-12 05:57:38 +0300

    Вернула контроллер как изначально,добавила конструктор

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -1,6 +1,9 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.comment.CommentDTO;
@@ -14,30 +17,40 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping("/{id}/comments")
-    public CommentsDTO getComments(Authentication authentication,
-                                   @PathVariable int id) {
-        return commentService.getComments(authentication, id);
+    public ResponseEntity<CommentsDTO> getComments(Authentication authentication,
+                                                   @PathVariable int id) {
+        CommentsDTO comments = commentService.getComments(authentication, id);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(comments);
     }
 
     @PostMapping("/{id}/comments")
-    public CommentDTO addComment(Authentication authentication,
-                                 @PathVariable int id,
-                                 @RequestBody CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
-        return commentService.addComment(authentication, id, createOrUpdateCommentDTO);
+    public ResponseEntity<CommentDTO> addComment(Authentication authentication,
+                                                 @PathVariable int id,
+                                                 @RequestBody CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
+        CommentDTO comment = commentService.addComment(authentication, id, createOrUpdateCommentDTO);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(comment);
     }
 
     @DeleteMapping("/{adId}/comments/{commentId}")
-    public void deleteComment(Authentication authentication,
-                              @PathVariable int adId,
-                              @PathVariable int commentId) {
+    public ResponseEntity<CommentDTO> deleteComment(Authentication authentication,
+                                                    @PathVariable int adId,
+                                                    @PathVariable int commentId) {
         commentService.deleteComment(authentication, adId, commentId);
+        return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{adId}/comments/{commentId}")
-    public CommentDTO updateComment(Authentication authentication,
-                                    @PathVariable int adId,
-                                    @PathVariable int commentId,
-                                    @RequestBody CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
-        return commentService.updateComment(authentication, adId, commentId, createOrUpdateCommentDTO);
+    public ResponseEntity<CommentDTO> updateComment(Authentication authentication,
+                                                    @PathVariable int adId,
+                                                    @PathVariable int commentId,
+                                                    @RequestBody CreateOrUpdateCommentDTO createOrUpdateCommentDTO) {
+        CommentDTO comment = commentService.updateComment(authentication, adId, commentId, createOrUpdateCommentDTO);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(comment);
     }
 }
--- src/main/java/ru/skypro/homework/dto/comment/CommentsDTO.java
+++ src/main/java/ru/skypro/homework/dto/comment/CommentsDTO.java
@@ -1,8 +1,15 @@
 package ru.skypro.homework.dto.comment;
 
-import java.util.List;
+import lombok.Data;
 
+import java.util.List;
+@Data
 public class CommentsDTO {
     private int count;
     private List<CommentDTO> results;
+
+    public CommentsDTO(int count){
+        this.count=count;
+    }
 }
+
