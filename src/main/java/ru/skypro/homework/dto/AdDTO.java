22224fa3be515ee2d8a1fa5220aa2a7776a16399
DmitriiGienko <cuanik@mail.ru>
2023-10-13 23:04:34 +0300

    создал adController, adDto, AdsDto, проекцию ExtendedAd, класс для получения заглушек, service&serviceImpl для Ad. завтра нужно разобраться с Patch rest

---
+++ src/main/java/ru/skypro/homework/dto/AdDTO.java
@@ -0,0 +1,19 @@
+package ru.skypro.homework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class AdDTO {
+
+    private int author; //id автора объявления
+
+    private String image;// ссылка на картинку объявления
+
+    private int pk; // id объявления
+
+    private int price; //цена объявления
+
+    private String title;//заголовок объявления
+}
---
+++ src/main/java/ru/skypro/homework/service/impl/AdServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.skypro.homework.service.impl;
+
+import ru.skypro.homework.dto.AdDTO;
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.projections.ExtendedAd;
+import ru.skypro.homework.service.AdService;
+
+import static ru.skypro.homework.service.ReturnableStubs.*;
+
+public class AdServiceImpl implements AdService {
+
+    @Override
+    public AdsDTO getAllAds() {
+        return getAdsDto();
+    }
+
+    @Override
+    public AdDTO addAd() {
+        return getListAdDTO().get(0);
+    }
+
+    @Override
+    public ExtendedAd getAdFullInfo(int id) {
+        return getExtendedAdd();
+    }
+
+    @Override
+    public AdsDTO updateAd() {
+        return null;
+    }
+
+    @Override
+    public void deleteAd() {
+    }
+
+    @Override
+    public AdsDTO getUserAdds() {
+        return getAdsDto();
+    }
+
+    @Override
+    public String updateImage() {
+        return "new path";
+    }
+}
