e59550667c5226ca79b2c8a2bf6d619817570295
Ilya <rabota003@yandex.ru>
2023-10-13 20:01:41 +0300

    Add DTO and controller + service

---
+++ src/main/java/ru/skypro/homework/service/CommentsServiceImpl.java
@@ -0,0 +1,35 @@
+package ru.skypro.homework.service;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.Comments;
+import ru.skypro.homework.dto.CreateOrUpdateComment;
+
+import javax.transaction.Transactional;
+
+
+@Transactional
+@Service
+public class CommentsServiceImpl implements CommentsService{
+
+    @Override
+    public Comments getFullComments(int id) {
+        return null;
+    }
+
+    @Override
+    public Comments addComments(int id) {
+        return null;
+    }
+
+    @Override
+    public void removeComments(int id, int commentsId) {
+
+    }
+
+    @Override
+    public CreateOrUpdateComment updateComments(int id, int commentsId) {
+        return null;
+    }
+
+
+}
