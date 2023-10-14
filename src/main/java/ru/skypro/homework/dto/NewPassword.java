90923f835d0fab1b0917c6650701722f54eba266
TV#BJJ <tv041000@gmail.com>
2023-10-15 00:26:00 +1000

    user + controller

---
+++ src/main/java/ru/skypro/homework/dto/NewPassword.java
@@ -0,0 +1,8 @@
+package ru.skypro.homework.dto;
+import lombok.Data;
+
+@Data
+public class NewPassword {
+    String currentPassword;
+    String newPassword;
+}
---
+++ src/main/java/ru/skypro/homework/dto/UpdateUser.java
@@ -0,0 +1,9 @@
+package ru.skypro.homework.dto;
+import lombok.Data;
+
+@Data
+public class UpdateUser {
+    String firstName;
+    String lastName;
+    String phone;
+}
