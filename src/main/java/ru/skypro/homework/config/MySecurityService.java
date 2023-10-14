b310582f0a712e3b8aa102aacdee085f6b93d51d
letoVlad <evtepo@gmail.com>
2023-10-14 13:15:03 +0300

    ref

--- src/main/java/ru/skypro/homework/config/MySecurityService.java
+++ src/main/java/ru/skypro/homework/config/MySecurityService.java
@@ -27,13 +27,11 @@ public class MySecurityService {
      * Проверяем, что пользователь удаляющий коментарий являеятся его создателем или АДМИНОМ
      */
     public boolean canDeleteComment(int commentId, int adId) {
-        checkForAdAndComment(adId, commentId);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Optional<CommentEntity> commentEntity = commentRepository.findById(commentId);
-        String name = commentEntity.get().getUser().getEmail();
+        CommentEntity commentEntity = checkForAdAndComment(adId, commentId);
+        String name = commentEntity.getUser().getEmail();
 
         if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
-            System.out.println(authentication.getAuthorities());
             return true;
         }
 
@@ -41,20 +39,14 @@ public class MySecurityService {
     }
 
     private CommentEntity checkForAdAndComment(int adId, int commentId) {
-        checkForAd(adId);
-        Optional<CommentEntity> commentEntity = commentRepository.findCommentByCommentIdAndAdId(adId, commentId);
+        adRepository.findById(adId).orElseThrow(() -> new NotFoundException(
+                String.format("Объявление с индексом \"%s\" не найдено.", adId)
+        ));
 
-        return commentEntity.orElseThrow(() -> new NotFoundException(String.format(
-                "Комментарий с индексом \"%s\" не найден для объявления с индексом \"%s\".",
-                commentId, adId))
-        );
+        return commentRepository.findCommentByCommentIdAndAdId(adId, commentId)
+                .orElseThrow(() -> new NotFoundException(String.format(
+                        "Комментарий с индексом \"%s\" не найден для объявления с индексом \"%s\".",
+                        commentId, adId))
+                );
     }
-
-    private AdEntity checkForAd(int adId) {
-        return adRepository.findById(adId)
-                .orElseThrow(() -> new NotFoundException(
-                        String.format("Объявление с индексом \"%s\" не найдено.", adId)
-                ));
-    }
-
 }
