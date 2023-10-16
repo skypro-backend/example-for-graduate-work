a64c32161f23da4358884aa254b2aeaa993a0587
PioneerX7 <vita-kopy@yandex.ru>
2023-10-16 21:50:54 +0300

    properties adn image entity

--- src/main/java/ru/skypro/homework/entity/Image.java
+++ src/main/java/ru/skypro/homework/entity/Image.java
@@ -1,4 +1,20 @@
 package ru.skypro.homework.entity;
 
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
+@Data
 public class Image {
+    @Id
+    private String id;
+
+    @Lob
+    @Column(name = "image")
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] bytes;
+
 }
