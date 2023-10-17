d8e1888152758bd13be5a0fd1470dbc4387ae3b8
Svetlana Morozova <svetlanochka@yandex.ru>
2023-10-17 13:32:09 +0300

    Marina 1 week

---
+++ src/main/java/ru/skypro/homework/dto/UpdateUser.java
@@ -0,0 +1,23 @@
+package ru.skypro.homework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.skypro.homework.entity.Users;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUser {
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public static UpdateUser toUpdateUser(Users users) {
+        UpdateUser updateUser = new UpdateUser();
+        updateUser.setPhone(users.getPhone());
+        updateUser.setFirstName(users.getFirstName());
+        updateUser.setLastName(users.getLastName());
+        return updateUser;
+    }
+}
---
+++ src/test/java/ru/skypro/homework/controller/UserControllerIntegrationTests.java
@@ -0,0 +1,127 @@
+package ru.skypro.homework.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import ru.skypro.homework.service.UserService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @WithMockUser(username = "user@gmail.com", roles = "USER", password = "password")
+    public void getInformation_status_isOk() throws Exception {
+        mockMvc.perform(get("/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value("user@gmail.com"));
+    }
+
+    @Test
+    public void getInformation_status_throw401() throws Exception {
+        mockMvc.perform(get("/users/me"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user@gmail.com", roles = "USER", password = "password")
+    public void setPassword_status_isOk() throws Exception {
+        JSONObject newPassword = new JSONObject();
+        newPassword.put("currentPassword", "password");
+        newPassword.put("newPassword", "password1");
+        mockMvc.perform(post("/users/set-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newPassword.toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user@gmail.com", roles = "USER", password = "password")
+    public void setPassword_status_isForbidden() throws Exception {
+        JSONObject newPassword = new JSONObject();
+        newPassword.put("currentPassword", "password1");
+        newPassword.put("newPassword", "password2");
+        mockMvc.perform(post("/users/set-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newPassword.toString()))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void setPassword_status_isUnauthorized() throws Exception {
+        JSONObject newPassword = new JSONObject();
+        newPassword.put("currentPassword", "password1");
+        newPassword.put("newPassword", "password2");
+        mockMvc.perform(post("/users/set-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newPassword.toString()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void updateInformationAboutUser_status_isUnauthorized() throws Exception {
+        JSONObject updateUser = new JSONObject();
+        updateUser.put("firstName", "ivan");
+        updateUser.put("lastName", "ivanova");
+        updateUser.put("phone", "+7(777)-777-77-77");
+        mockMvc.perform(patch("/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateUser.toString()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "user@gmail.com", roles = "USER", password = "password")
+    public void updateInformationAboutUser_status_isOk() throws Exception {
+        JSONObject updateUser = new JSONObject();
+        updateUser.put("firstName", "ivan");
+        updateUser.put("lastName", "ivanova");
+        updateUser.put("phone", "+7(777)-777-77-77");
+        mockMvc.perform(patch("/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateUser.toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user@gmail.com", roles = "USER", password = "password")
+    public void updateImage_status_isOk() throws Exception {
+        JSONObject image = new JSONObject();
+        image.put("newImage", "path-for-image");
+
+        mockMvc.perform(patch("/users/me/image")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(image.toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateImage_status_isUnauthorized() throws Exception {
+        JSONObject image = new JSONObject();
+        image.put("newImage", "path-for-image");
+
+        mockMvc.perform(patch("/users/me/image")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(image.toString()))
+                .andExpect(status().isUnauthorized());
+    }
+
+
+
+
+}
