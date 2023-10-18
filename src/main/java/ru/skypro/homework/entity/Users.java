d034e779f863cf64ef4394725c2e8400afafd59d
MarinaSulaeva <118995173+MarinaSulaeva@users.noreply.github.com>
2023-10-18 17:40:51 +0600

    добавлена entity users, userrepository, changelog для liquibase

--- src/main/java/ru/skypro/homework/entity/Users.java
+++ src/main/java/ru/skypro/homework/entity/Users.java
@@ -1,22 +1,40 @@
 package ru.skypro.homework.entity;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 import ru.skypro.homework.dto.Role;
+
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
-//этот класс станет entity
+@Entity
+@Table(name = "users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String email;
+
     private String image;
+
+    @Size(min=4, max=32)
     private String username;
+
+    @Size(min=8, max=16)
     private String password;
+
+    @Size(min=2, max=16)
     private String firstName;
+
+    @Size(min=2, max=16)
     private String lastName;
+    @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}")
     private String phone;
+
     private Role role;
 }
