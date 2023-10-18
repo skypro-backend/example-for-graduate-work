a6e48dac81128347059243c4789513ec33e24d64
Ilya <rabota003@yandex.ru>
2023-10-18 13:00:40 +0300

    Chahges DTO

--- src/main/java/ru/skypro/homework/service/impl/CommentsServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/CommentsServiceImpl.java
@@ -1,13 +1,10 @@
 package ru.skypro.homework.service.impl;
 
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
+
 import ru.skypro.homework.dto.CommentsDTO;
 import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
-=======
-import ru.skypro.homework.dto.Comments;
-import ru.skypro.homework.dto.CreateOrUpdateComment;
->>>>>>> origin/Change
+
 import ru.skypro.homework.service.CommentsService;
 
 import javax.transaction.Transactional;
@@ -18,20 +15,16 @@ import javax.transaction.Transactional;
 public class CommentsServiceImpl implements CommentsService {
 
     @Override
-<<<<<<< HEAD
+
     public CommentsDTO getFullComments(int id) {
-=======
-    public Comments getFullComments(int id) {
->>>>>>> origin/Change
+
         return null;
     }
 
     @Override
-<<<<<<< HEAD
+
     public CommentsDTO addComments(int id) {
-=======
-    public Comments addComments(int id) {
->>>>>>> origin/Change
+
         return null;
     }
 
@@ -41,11 +34,9 @@ public class CommentsServiceImpl implements CommentsService {
     }
 
     @Override
-<<<<<<< HEAD
+
     public CreateOrUpdateCommentDTO updateComments(int id, int commentsId) {
-=======
-    public CreateOrUpdateComment updateComments(int id, int commentsId) {
->>>>>>> origin/Change
+
         return null;
     }
 
