9d41303c80fe28a8846075df7c9c6deafc4c810b
anna <anna@sayf.su>
2023-10-18 22:37:30 +0300

    добавлен маппинг и liquibase

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -3,12 +3,14 @@ package ru.skypro.homework.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.skypro.homework.dto.ads.Ads;
 import ru.skypro.homework.entity.Ad;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AdsRepository extends CrudRepository<Ad, Integer> {
 
 
