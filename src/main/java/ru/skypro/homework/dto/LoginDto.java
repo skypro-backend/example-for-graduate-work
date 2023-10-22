7ab9acce03e70dfb1c687fe8857b09454ee40d51
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-10-22 18:44:09 +0400

    #5 Int за место Long

--- src/main/java/ru/skypro/homework/dto/LoginDto.java
+++ src/main/java/ru/skypro/homework/dto/LoginDto.java
@@ -1,12 +1,5 @@
 package ru.skypro.homework.dto;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-@NoArgsConstructor(force = true)
 public record LoginDto(
         /**
          minLength: 4, maxLength: 16
