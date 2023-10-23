e536f70b6b700b6a7ace2e5c3399111f444a205f
Elena Koroleva <koroliana@yandex.ru>
2023-10-24 00:22:45 +0700

    Подкорректированы контроллеры в соответсвии с API Код контроллеров приведен к единому виду
    Приведение кода контроллеров к единому виду

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,19 +1,17 @@
 package ru.skypro.homework.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.SneakyThrows;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.AdDto;
 import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.dto.CreateOrUpdateAdDto;
+import ru.skypro.homework.dto.ExtendedAdDto;
 
-import javax.validation.Valid;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -38,35 +36,33 @@ public class AdsController {
     @Operation(summary = "Получение всех объявлений",
             description = "Получение количества и списка всех объявлений")
     @ApiResponse(responseCode = "200", description = "OK")
-    @ApiResponse(responseCode = "204", description = "No Content")
-    @ApiResponse(responseCode = "404", description = "Not Found")
     public ResponseEntity<AdsDto> getAllAds() {
-        AdsDto AdsDtoList = new AdsDto(0, new ArrayList<>());
-        return ResponseEntity.ok(AdsDtoList);
+        AdsDto allAdsDtoList = new AdsDto(0, new ArrayList<>());
+        return ResponseEntity.ok(allAdsDtoList);
     }
 
-    @SneakyThrows
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Добавление объявления",
             description = "Добавление изображения и всех полей объявления")
-    @ApiResponse(responseCode = "200", description = "OK")
+    @ApiResponse(responseCode = "201", description = "Created")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
-    @ApiResponse(responseCode = "404", description = "Not found")
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<AdsDto> addAds(@Parameter(description = "Данные нового объявления")
-                                         @RequestPart("image") MultipartFile imageFile,
-                                         @Valid @RequestPart("properties") AdsDto createAdsDto, Authentication authentication) {
-        return ResponseEntity.ok(createAdsDto);
+    public ResponseEntity<AdDto> addAd(@RequestBody CreateOrUpdateAdDto createOrUpdateAdDto,
+                                       @RequestParam MultipartFile image) throws IOException {
+        AdDto addedAdDto = new AdDto(1, "imagePath", 1, createOrUpdateAdDto.price(), createOrUpdateAdDto.title());
+        return ResponseEntity.ok(addedAdDto);
     }
 
+    @GetMapping("/{id}")
     @Operation(summary = "Получение информации об объявлении",
             description = "Получение информации об объявлении по его id")
     @ApiResponse(responseCode = "200", description = "OK")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "404", description = "Not found")
-    @GetMapping("/{adId}")
-    public ResponseEntity<AdsDto> getAuthById(@PathVariable("adId") Integer adId,
-                                              @RequestBody AdsDto adsDto) {
-        return ResponseEntity.ok(adsDto);
+    public ResponseEntity<ExtendedAdDto> getAuthById(@PathVariable("id") Integer id) {
+        ExtendedAdDto extendedAdDto = new ExtendedAdDto(id, "authorFirstName",
+                "authorLastName", "description", "email", "imagePath",
+                "phone", 100, "title");
+        return ResponseEntity.ok(extendedAdDto);
     }
 
     @PatchMapping("/{id}")
@@ -77,7 +73,9 @@ public class AdsController {
     @ApiResponse(responseCode = "403", description = "Forbidden")
     @ApiResponse(responseCode = "404", description = "Not found")
     public ResponseEntity<AdDto> updateAd(@PathVariable("id") Integer id,
-                                          @RequestBody AdDto updatedAdDto) {
+                                          @RequestBody CreateOrUpdateAdDto createOrUpdateAdDto) {
+        AdDto updatedAdDto = new AdDto(1, "imagePath",
+                id, createOrUpdateAdDto.price(), createOrUpdateAdDto.title());
         return ResponseEntity.ok(updatedAdDto);
     }
 
@@ -86,8 +84,8 @@ public class AdsController {
     @ApiResponse(responseCode = "200", description = "OK")
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     public ResponseEntity<AdsDto> getAuthorizedUserAds() {
-        AdsDto list = new AdsDto(0, new ArrayList<>());
-        return ResponseEntity.ok(list);
+        AdsDto userAdsDtoList = new AdsDto(0, new ArrayList<>());
+        return ResponseEntity.ok(userAdsDtoList);
     }
 
     @PatchMapping(value = "/{id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -99,8 +97,7 @@ public class AdsController {
     @ApiResponse(responseCode = "404", description = "Not found")
     public ResponseEntity<String> updateAdImage(@PathVariable("id") Integer id,
                                                 @RequestParam MultipartFile image) throws IOException {
-        String result = "Успешно";
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok("Image updated successfully");
     }
 
 }
