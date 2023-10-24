9879f6e7eaae08149ce82dcc5384e0646dd7d33c
loudviper8815726 <george2066@yandex.ru>
2023-10-24 23:12:13 +0300

    registration(fixed)

--- src/main/java/ru/skypro/homework/controller/RegisterController.java
+++ src/main/java/ru/skypro/homework/controller/RegisterController.java
@@ -3,16 +3,21 @@ package ru.skypro.homework.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.RegisterDto;
 import ru.skypro.homework.service.RegisterService;
 
-@Controller
+@RestController
+@RequestMapping(path = "/register")
+@CrossOrigin(value = "http://localhost:3000")
 @AllArgsConstructor
 public class RegisterController {
     private final RegisterService registerService;
+
+    public RegisterController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterDto register) {
         if (registerService.registerUser(register)) {
