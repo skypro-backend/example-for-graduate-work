9c1f25796d8c638ba5057f5b82600ca623dfef7f
Gleb Lykov <61714086+glykov@users.noreply.github.com>
2023-10-25 21:32:52 +0300

    Delete graduate-work directory

--- graduate-work/src/main/java/ru/skypro/homework/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package ru.skypro.homework.service;
-
-import ru.skypro.homework.dto.Register;
-
-public interface AuthService {
-    boolean login(String userName, String password);
-
-    boolean register(Register register);
-}
