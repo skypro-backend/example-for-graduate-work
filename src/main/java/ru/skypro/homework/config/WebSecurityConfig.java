0cbadb573c46f9eea3d7320edbac40f25c09d8e8
DmitriiGienko <cuanik@mail.ru>
2023-10-25 21:21:29 +0300

    Авторизация и регистрация готова!!!

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -25,17 +25,17 @@ public class WebSecurityConfig {
             "/register"
     };
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user =
-                User.builder()
-                        .username("user@gmail.com")
-                        .password("password")
-                        .passwordEncoder(passwordEncoder::encode)
-                        .roles(Role.USER.name())
-                        .build();
-        return new InMemoryUserDetailsManager(user);
-    }
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+//        UserDetails user =
+//                User.builder()
+//                        .username("user@gmail.com")
+//                        .password("password")
+//                        .passwordEncoder(passwordEncoder::encode)
+//                        .roles(Role.USER.name())
+//                        .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- src/main/java/ru/skypro/homework/repository/UserRepo.java
+++ src/main/java/ru/skypro/homework/repository/UserRepo.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.model.UserModel;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<UserModel, Integer> {
+    Optional<UserModel> findByUserName(String username);
 //    Optional<UserModel> findByFirstName(String firstName);
 }
