a16b68cd82d6c62d6eeb5d479eb5d01d4c9b6cd7
loudviper8815726 <george2066@yandex.ru>
2023-10-25 19:25:15 +0300

    advertisement

---
+++ src/main/java/ru/skypro/homework/service/impl/AdvertisementServiceImpl.java
@@ -0,0 +1,30 @@
+package ru.skypro.homework.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.entity.Advertisement;
+import ru.skypro.homework.repository.AdvertisementRepository;
+import ru.skypro.homework.service.AdvertisementService;
+
+@Service
+@AllArgsConstructor
+public class AdvertisementServiceImpl implements AdvertisementService {
+    private final AdvertisementRepository repository;
+
+    @Override
+    public void create(Advertisement advertisement) {
+        repository.save(advertisement);
+    }
+
+    @Override
+    public void update(Advertisement advertisement) {
+        repository.delete(advertisement);
+        repository.save(advertisement);
+    }
+
+    @Override
+    public void delete(Advertisement advertisement) {
+        repository.delete(advertisement);
+    }
+}
