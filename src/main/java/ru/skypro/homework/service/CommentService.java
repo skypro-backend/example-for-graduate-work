af27c9fc2c68640600418496c6400ba9da264238
Alex <shurik228154@mail.ru>
2023-10-27 23:38:16 +0300

    add comment: entity, repository, service

---
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -0,0 +1,26 @@
+package ru.skypro.homework.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.dto.CreateOrUpdateComment;
+import ru.skypro.homework.entity.CommentEntity;
+import ru.skypro.homework.entity.UserEntity;
+
+@Slf4j
+@Service
+public class CommentService {
+    private ModelMapper mapper;
+
+    private CommentEntity convertToCommentEntity(Comment comment){
+        return mapper.map(comment,CommentEntity.class);
+    }
+    private Comment convertToCommentDto(CommentEntity commentEntity, UserEntity userEntity){
+        return mapper.map(commentEntity, Comment.class);
+    }
+    private CommentEntity convertToCommentEntity(CreateOrUpdateComment comment){
+        return mapper.map(comment,CommentEntity.class);
+    }
+
+}
