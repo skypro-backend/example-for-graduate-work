b4ce6450cb6ebb850fdf601e252ed5f57a3eb236
Elena Koroleva <koroliana@yandex.ru>
2023-10-29 17:46:25 +0700

    Реализован эндпоинт на вывод всех комментов Маппер возвращен в абстрактный класс

--- src/main/java/ru/skypro/homework/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -3,6 +3,7 @@ package ru.skypro.homework.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.skypro.homework.dto.CommentDto;
+import ru.skypro.homework.dto.CommentsDto;
 import ru.skypro.homework.dto.CreateOrUpdateCommentDto;
 import ru.skypro.homework.entity.Ad;
 import ru.skypro.homework.entity.Comment;
@@ -10,32 +11,37 @@ import ru.skypro.homework.entity.User;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 
 @Mapper
-public interface CommentMapper {
+public abstract class CommentMapper {
 
     @Mapping(target = "author", source = "user.id")
     @Mapping(target = "authorImage", source = "user.userImage.filePath")
     @Mapping(target = "authorFirstName", source = "user.firstName")
     @Mapping(target = "createdAt", expression = "java(getLongFromLocalDateTime(comment.getCreatedAt()))")
     @Mapping(target = "pk", source = "id")
-    CommentDto entityToCommentDto(Comment comment);
+    public abstract CommentDto entityToCommentDto(Comment comment);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "user", source = "user")
     @Mapping(target = "ad", source = "ad")
-    Comment createCommentDtoToEntity(Ad ad, CreateOrUpdateCommentDto createOrUpdateCommentDto, User user);
+    public abstract Comment createCommentDtoToEntity(Ad ad, CreateOrUpdateCommentDto createOrUpdateCommentDto, User user);
 
     @Mapping(target = "id", source = "commentId")
     @Mapping(target = "createdAt", source = "createdAt")
     @Mapping(target = "user", source = "user")
     @Mapping(target = "ad", source = "ad")
-    Comment updateCommentDtoToEntity(Ad ad, Integer commentId,
-                                     CreateOrUpdateCommentDto createOrUpdateCommentDto,
-                                     LocalDateTime createdAt, User user);
+    public abstract Comment updateCommentDtoToEntity(Ad ad, Integer commentId,
+                                                     CreateOrUpdateCommentDto createOrUpdateCommentDto,
+                                                     LocalDateTime createdAt, User user);
 
-    default Long getLongFromLocalDateTime(LocalDateTime localDateTime) {
+    public CommentsDto commentDtoListToCommentsDto(List<CommentDto> commentDtoList) {
+        return new CommentsDto(commentDtoList.size(), commentDtoList);
+    }
+
+    protected Long getLongFromLocalDateTime(LocalDateTime localDateTime) {
         return localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
     }
 
