b050d93ab2cf7d8c5dde49d76f7bc0598c537ac0
Sasha <alex_penza@list.ru>
2023-10-29 12:54:53 +0200

    ad mappers

--- src/main/java/ru/skypro/homework/service/AdsService.java
+++ src/main/java/ru/skypro/homework/service/AdsService.java
@@ -1,41 +1,48 @@
 package ru.skypro.homework.service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.Ad;
 import ru.skypro.homework.dto.Ads;
 import ru.skypro.homework.dto.CreateOrUpdateAd;
+import ru.skypro.homework.entity.AdEntity;
+import ru.skypro.homework.repository.AdRepository;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class AdsService {
     private static Logger logger = LoggerFactory.getLogger(AdsService.class);
+    private final ModelMapper mapper;
+    private final AdRepository adRepository;
 
-    private List<Ad> testAds = new ArrayList<>(); //Replace with Ads repo
+    private AdEntity adToAdEntity(Ad ad) {
+        return mapper.map(ad, AdEntity.class);
+    }
 
-    public Ad addNewAd(CreateOrUpdateAd createOrUpdateAd) {
+    private Ad adEntityToAd(AdEntity adEntity) {
+        return mapper.map(adEntity, Ad.class);
+    }
+
+    public Ad addAd(CreateOrUpdateAd createOrUpdateAd) {
         logger.info("Ad was successfully added: {}", createOrUpdateAd.getTitle());
-        return new Ad();
+        return new Ad(); // Temp
     }
 
     public Ads getAllAds() {
-        if (testAds.isEmpty()) {
-            Ad ad = new Ad();
-            ad.setTitle("Title 1");
-            ad.setPrice(3333);
-            ad.setTitle("Title 1");
-            ad.setPk(555);
-            ad.setAuthor(111);
-            testAds.add(ad);
-        }
-        Ads ads = new Ads();//Replace with mapper
-        ads.setCount(testAds.size());
-        ads.setResults(testAds);
+        List<Ad> allAd = adRepository.findAll().stream().map(a -> mapper.map(a, Ad.class)).collect(Collectors.toList());
+        Ads ads = new Ads();
+        ads.setCount(allAd.size());
+        ads.setResults(allAd);
         logger.info("Number of ads sent: {}", ads.getCount());
         return ads;
     }
