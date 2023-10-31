54c730ec4ec03530c9cdb59912b0f36a23d56f3b
Gleb Lykov <glebdom@gmail.com>
2023-10-31 22:33:29 +0300

    start working on UserService

--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ru.skypro.homework.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.skypro.homework.entity.UserEntity;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
 }
