baf38b7b6b8972782ebacf190ba91171685ded98
Alex <shurik228154@mail.ru>
2023-11-07 21:19:33 +0300

    ResponseStatus instead of try/catch

--- src/main/java/ru/skypro/homework/exceptions/AdNotFoundException.java
+++ src/main/java/ru/skypro/homework/exceptions/AdNotFoundException.java
@@ -1,5 +1,9 @@
 package ru.skypro.homework.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AdNotFoundException extends RuntimeException{
     public AdNotFoundException() {
         super();
