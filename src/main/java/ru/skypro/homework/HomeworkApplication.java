a030e0a851be81f8f21f68fefaa5e1c1ef6c54f9
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-11-14 15:57:03 +1000

    init commit - сделала fork проекта и добавил ветки dev и week1

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomeworkApplication {
   public static void main(String[] args) {
     SpringApplication.run(HomeworkApplication.class, args);
+
   }
 
 }
