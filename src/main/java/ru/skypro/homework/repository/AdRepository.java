c1564a2c4827187ffb1a79e39e866024b60b51bd
Legion <guritch21@gmail.com>
2023-11-15 22:11:21 +0300

    added CommentRepository and AdRepository

---
+++ src/main/java/ru/skypro/homework/repository/AdRepository.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.skypro.homework.model.Ad;
+import ru.skypro.homework.model.User;
+
+import java.util.List;
+
+public interface AdRepository extends JpaRepository <Ad, Long> {
+    List<Ad> findByAuthor(User author);
+}
