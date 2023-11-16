7f92043d16b66a4752357a209bcb84e49cd8be65
kirill <freelook22@mail.ru>
2023-11-16 20:25:05 +0300

    AuthController без ошибок

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -20,6 +20,10 @@ public class AuthController {
 
     private final AuthService authService;
 
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Login login) {
         if (authService.login(login.getUsername(), login.getPassword())) {
