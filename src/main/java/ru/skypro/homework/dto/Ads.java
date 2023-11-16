c32d99b79842e428b4d171ef9bf3f9adfd69593c
kirill <freelook22@mail.ru>
2023-11-16 17:30:50 +0300

    все DTO

---
+++ src/main/java/ru/skypro/homework/dto/Ads.java
@@ -0,0 +1,10 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Ads {
+    private int count;
+    private List<Ad> results;
+}
---
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateComment.java
@@ -0,0 +1,8 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateOrUpdateComment {
+    private String text;
+}
