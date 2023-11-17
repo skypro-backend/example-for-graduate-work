20c7ad79cedc72f443813fd948c2e0022bc750dd
Legion <guritch21@gmail.com>
2023-11-17 17:15:49 +0300

    added "/ads" in AUTH_WHITELIST

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -18,7 +18,8 @@ public class WebSecurityConfig {
             "/v3/api-docs",
             "/webjars/**",
             "/login",
-            "/register"
+            "/register",
+            "/ads"
     };
 
     @Bean
