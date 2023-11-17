ce2c03d2b83bab337de197d655374898d3825cdf
Legion <guritch21@gmail.com>
2023-11-17 13:30:21 +0300

    implementation of authorization from the database using MyUserDetailsService

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -3,23 +3,14 @@ package ru.skypro.homework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.skypro.homework.repository.UserRepository;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfig {
-    private final UserRepository userRepository;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
@@ -29,32 +20,6 @@ public class WebSecurityConfig {
             "/login",
             "/register"
     };
-
-    public WebSecurityConfig(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-       /* UserDetails user =
-                User.builder()
-                        .username("user@gmail.com")
-                        .password("password")
-                        .passwordEncoder(passwordEncoder::encode)
-                        .roles(Role.USER.name())
-                        .build();*/
-        return new InMemoryUserDetailsManager(getUserDetails());
-    }
-
-    private List<UserDetails> getUserDetails() {
-        List<UserDetails> result = new ArrayList<>();
-        userRepository.findAll().forEach(e -> result.add(User.builder()
-                .username(e.getEmail())
-                .password(e.getPassword())
-                .roles(e.getRole().name())
-                .build()));
-        return result;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
