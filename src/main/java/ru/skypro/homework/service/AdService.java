8882c3fb4884116111888d51564a3a72951b33ed
vpm789 <vpm789@gmail.com>
2023-11-19 00:20:19 +0300

    add ad

---
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -0,0 +1,21 @@
+package ru.skypro.homework.service;
+
+import ru.skypro.homework.dto.CreateOrUpdateAd;
+import ru.skypro.homework.model.AdEntity;
+import ru.skypro.homework.model.UserEntity;
+
+public interface AdService {
+
+    /**
+     * Метод добавляет товар.
+     * @return объект {@link AdEntity}, содержащий информацию о товаре.
+     */
+    AdEntity createAd(AdEntity createAd);
+
+    /**
+     * Метод обновляет информацию о товаре.
+     * @param updateAd объект содержащий поля с заголовком, ценой и описанием товара.
+     * @return объект {@link AdEntity}, содержащий информацию о товаре.
+     */
+    AdEntity updateAd(AdEntity updateAd);
+}
