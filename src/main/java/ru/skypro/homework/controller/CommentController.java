39fba8e63e0e17785d84280bd56611131763a84d
t3hVega <ivan.laganchuk@gmail.com>
2023-11-18 18:17:58 +0300

    DTO, контроллеры

---
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -0,0 +1,51 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Comment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    @GetMapping("/ads/{adId}/comments")
+    public ResponseEntity<List<Comment>> getComments(@PathVariable long adId) {
+        HttpHeaders headers = new HttpHeaders();
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(Arrays.asList(
+                new Comment()
+        ));
+    }
+
+    @PostMapping("/ads/{adId}/comments")
+    public ResponseEntity<Comment> postComment(@PathVariable long adId,
+                                               @RequestBody Comment comment) {
+        HttpHeaders headers = new HttpHeaders();
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(new Comment());
+    }
+
+    @DeleteMapping("/ads/{adId}/comments/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable long adId,
+                                              @PathVariable long commentId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/ads/{adId}/comments/{commentId}")
+    public ResponseEntity<Comment> updateComment(@PathVariable long adId,
+                                                 @PathVariable long commentId,
+                                                 @RequestBody Comment comment) {
+        HttpHeaders headers = new HttpHeaders();
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(new Comment());
+    }
+}
---
+++ src/main/java/ru/skypro/homework/dto/Image.java
@@ -0,0 +1,83 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Data
+public class Image {
+    private Long id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    private byte[] data;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return fileSize == image.fileSize && Objects.equals(id, image.id) && Objects.equals(filePath, image.filePath) && Objects.equals(mediaType, image.mediaType) && Arrays.equals(data, image.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, filePath, fileSize, mediaType);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", filePath='" + filePath + '\'' +
+                ", fileSize=" + fileSize +
+                ", mediaType='" + mediaType + '\'' +
+                ", data=" + Arrays.toString(data) +
+                '}';
+    }
+}
