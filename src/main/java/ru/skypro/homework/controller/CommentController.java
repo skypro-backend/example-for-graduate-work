389fe7c23d612bd9d4afe4ef8fd3d762723cb0b8
vpm789 <vpm789@gmail.com>
2023-11-27 00:50:02 +0300

    работа над методами удаления и обновления комментов

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -42,27 +42,42 @@ public class CommentController {
     }
 
     @DeleteMapping("/{adId}/comments/{commentId}")
-    public ResponseEntity deleteComment(@PathVariable("adId") Integer adId,
-                                        @PathVariable("commentId") Integer commentId){
-        //todo добавить условие - админ может удалять любые комменты, юзер - только свои.
+    public ResponseEntity<?> deleteComment(@PathVariable("commentId") Integer commentId) {
+        //
         //как сделать: нужно найти в БД юзера по его логину из authService.getLogin()
         //и проверить его статус: админ или юзер, если админ то удаляем коммент. если нет, то см. далее
         //Далее нужно найти коммент и проверить кому он принадлежит, если текущему юзеру (он не админ),
-        //то удаляем, если нет, то статус 403.
-        if(authService.getLogin() != null){
-            return ResponseEntity.ok(commentService.deleteComment(adId, commentId));
-        }else {
+        //то удаляем, если нет, то статус 403.todo добавить условие - админ может удалять любые комменты, юзер - только свои.
+        //Нужен ли adId ?
+        if (authService.getLogin() != null) {
+            String result = commentService.deleteComment(commentId, authService.getLogin().getUsername());
+            if (result.equals("forbidden")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            } else if (result.equals("not found")) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            } else {
+                return ResponseEntity.status(HttpStatus.OK).build();
+            }
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
+
     @PatchMapping("/{adId}/comments/{commentId}")
-    public ResponseEntity<Comment> updateComment(@PathVariable("adId") Integer adId,
-                                        @PathVariable("commentId") Integer commentId,
-                                        @RequestBody CreateOrUpdateComment createOrUpdateComment){
+    public ResponseEntity<Comment> updateComment(@PathVariable("commentId") Integer commentId,
+                                                 @RequestBody CreateOrUpdateComment createOrUpdateComment) {
         //todo добавить условие, только пользователь написавший коммент может его править.
-        if(authService.getLogin() != null){
-            return ResponseEntity.ok(commentService.updateComment(adId, commentId, createOrUpdateComment));
-        }else {
+        //Нужен ли adId ?
+        if (authService.getLogin() != null) {
+            Comment comment = commentService.updateComment(commentId, createOrUpdateComment, authService.getLogin().getUsername());
+            if (comment.getText().equals(createOrUpdateComment.getText())) {
+                return ResponseEntity.ok(comment);
+            } else if (comment == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
