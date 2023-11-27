780d09554163c94921a0ae9e434cbffbcc2ab719
t3hVega <123067041+t3hVega@users.noreply.github.com>
2023-11-27 18:56:37 +0300

    Delete src/main/java/ru/skypro/homework/service/AdService.java

--- src/main/java/ru/skypro/homework/service/AdService.java
+++
@@ -1,9 +0,0 @@
-package ru.skypro.homework.service;
-
-import ru.skypro.homework.dto.RegisterDto;
-
-public interface AdService {
-    boolean login(String userName, String password);
-
-    boolean register(RegisterDto registerDto);
-}
