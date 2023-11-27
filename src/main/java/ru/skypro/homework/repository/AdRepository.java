5f671fa8c47cd85fd386cb7ccdbcd38e631f8543
t3hVega <123067041+t3hVega@users.noreply.github.com>
2023-11-27 18:43:50 +0300

    Delete src/main/java/ru/skypro/homework/repository/AdRepository.java

--- src/main/java/ru/skypro/homework/repository/AdRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.dto.Ad;
-
-public interface AdRepository extends JpaRepository<Ad,Integer> {
-}
