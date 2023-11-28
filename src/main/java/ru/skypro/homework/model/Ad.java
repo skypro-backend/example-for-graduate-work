76201f29fe3b6a16142909364fc52f92bc488442
t3hVega <ivan.laganchuk@gmail.com>
2023-11-28 03:27:20 +0300

    Добавление таблицы обьявлений

--- src/main/java/ru/skypro/homework/model/Ad.java
+++ src/main/java/ru/skypro/homework/model/Ad.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "ads")
 public class Ad {
 
     private User author;
