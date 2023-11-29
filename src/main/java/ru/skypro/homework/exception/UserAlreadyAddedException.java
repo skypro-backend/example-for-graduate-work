4d85ba76848e5c2cfbe7358b37fb71c9258e26d1
warlocktony <antonz3594@gmail.com>
2023-11-29 19:30:55 +0300

    test777

--- src/main/java/ru/skypro/homework/exception/UserAlreadyAddedException.java
+++ src/main/java/ru/skypro/homework/exception/UserAlreadyAddedException.java
@@ -1,7 +1,7 @@
 package ru.skypro.homework.exception;
 
-public class UserAlreadyAddedException extends RuntimeException{
-    public UserAlreadyAddedException(String message){
+public class UserAlreadyAddedException extends RuntimeException {
+    public UserAlreadyAddedException(String message) {
         super(message);
     }
 }
