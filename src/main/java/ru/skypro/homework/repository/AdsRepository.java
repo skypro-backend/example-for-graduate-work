80e340ba2e3c6636a9939e900ddf37a6bf219a2b
warlocktony <antonz3594@gmail.com>
2023-11-29 19:33:48 +0300

    test77777

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.models.Ads;
-
-public interface AdsRepository extends JpaRepository<Ads, Long> {
-}
