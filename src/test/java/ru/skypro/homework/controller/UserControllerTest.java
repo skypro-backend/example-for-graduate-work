b9717003f76cf3446733dd6c2fce60ab4d6eb5dd
Arthur <guritch21@gmail.com>
2023-12-02 01:00:17 +0300

    corrected test

--- src/test/java/ru/skypro/homework/controller/UserControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/UserControllerTest.java
@@ -1,15 +1,14 @@
 package ru.skypro.homework.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,16 +21,14 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDTO;
 import ru.skypro.homework.dto.UpdateUserDTO;
 import ru.skypro.homework.dto.UserDTO;
-import ru.skypro.homework.repository.ImageRepository;
 import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.UserService;
-import ru.skypro.homework.service.impl.ImageServiceImpl;
-import ru.skypro.homework.service.impl.UserServiceImpl;
 
 import java.io.InputStream;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ContextConfiguration(classes = {UserController.class})
@@ -39,9 +36,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest {
     @Autowired
     private UserController userController;
-
     @MockBean
     private UserService userService;
+
 
     /**
      * Method under test:  {@link UserController#updateUser(UpdateUserDTO)}
@@ -136,36 +133,17 @@ class UserControllerTest {
      * Method under test: {@link UserController#setUserPassword(NewPasswordDTO)}
      */
     @Test
-    @Disabled("TODO: Complete this test")
     void testSetUserPassword() {
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   org.springframework.http.converter.HttpMessageConversionException: Type definition error: [simple type, class ru.skypro.homework.dto.NewPasswordDTO]; nested exception is com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `ru.skypro.homework.dto.NewPasswordDTO` (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
-        //    at [Source: (org.springframework.util.StreamUtils$NonClosingInputStream); line: 1, column: 2]
-        //       at javax.servlet.http.HttpServlet.service(HttpServlet.java:555)
-        //       at javax.servlet.http.HttpServlet.service(HttpServlet.java:623)
-        //   com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `ru.skypro.homework.dto.NewPasswordDTO` (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
-        //    at [Source: (org.springframework.util.StreamUtils$NonClosingInputStream); line: 1, column: 2]
-        //       at com.fasterxml.jackson.databind.exc.InvalidDefinitionException.from(InvalidDefinitionException.java:67)
-        //       at com.fasterxml.jackson.databind.DeserializationContext.reportBadDefinition(DeserializationContext.java:1904)
-        //       at com.fasterxml.jackson.databind.DatabindContext.reportBadDefinition(DatabindContext.java:400)
-        //       at com.fasterxml.jackson.databind.DeserializationContext.handleMissingInstantiator(DeserializationContext.java:1349)
-        //       at com.fasterxml.jackson.databind.deser.BeanDeserializerBase.deserializeFromObjectUsingNonDefault(BeanDeserializerBase.java:1415)
-        //       at com.fasterxml.jackson.databind.deser.BeanDeserializer.deserializeFromObject(BeanDeserializer.java:352)
-        //       at com.fasterxml.jackson.databind.deser.BeanDeserializer.deserialize(BeanDeserializer.java:185)
-        //       at com.fasterxml.jackson.databind.deser.DefaultDeserializationContext.readRootValue(DefaultDeserializationContext.java:323)
-        //       at com.fasterxml.jackson.databind.ObjectMapper._readMapAndClose(ObjectMapper.java:4674)
-        //       at com.fasterxml.jackson.databind.ObjectMapper.readValue(ObjectMapper.java:3682)
-        //       at javax.servlet.http.HttpServlet.service(HttpServlet.java:555)
-        //       at javax.servlet.http.HttpServlet.service(HttpServlet.java:623)
-        //   See https://diff.blue/R013 to resolve this issue.
+        // Мокаем userService.setPassword, чтобы он ничего не делал
+        doNothing().when(userService).setPassword(any());
 
-        Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
-        UserRepository userRepository = mock(UserRepository.class);
-        UserController userController = new UserController(
-                new UserServiceImpl(encoder, userRepository, new ImageServiceImpl(mock(ImageRepository.class))));
-        userController.setUserPassword(new NewPasswordDTO("iloveyou", "iloveyou"));
+        // Вызываем метод контроллера
+        ResponseEntity<?> responseEntity = userController.setUserPassword(new NewPasswordDTO("oldPassword", "newPassword"));
+
+        // Проверяем, что возвращается ожидаемый ResponseEntity
+        assertEquals(200, responseEntity.getStatusCodeValue());
+
+        // Проверяем, что userService.setPassword вызывается с правильным параметром
+        verify(userService).setPassword(any());
     }
 }
