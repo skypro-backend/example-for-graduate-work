bf7ac89977c11a463458f98968d0e29b8847e673
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-12-07 20:53:59 +1000

    проект запускается после рефакторинга. но нужно поискать ошибки

--- src/main/java/ru/skypro/homework/model/PhotoEntity.java
+++ src/main/java/ru/skypro/homework/model/PhotoEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Table(name = "photos")
 public class PhotoEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String filePath;
     private long fileSize;
