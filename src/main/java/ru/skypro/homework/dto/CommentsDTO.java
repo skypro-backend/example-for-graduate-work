773aa9c4e274b22295ab21ab57a651c26e174998
HellHorseman <duke.sheffer@yandex.ru>
2023-12-07 22:00:49 +0400

    fix DTO Info & Comments

---
+++ src/main/java/ru/skypro/homework/dto/CommentsDTO.java
@@ -0,0 +1,14 @@
+package ru.skypro.homework.dto;
+
+
+public class CommentsDTO {
+
+    private Long id;
+    private String authorImage;
+    private String firstName;
+    private int createdAt;
+    private Long commentId;
+    private String text;
+
+
+}
---
+++ src/main/java/ru/skypro/homework/dto/InfoDTO.java
@@ -0,0 +1,19 @@
+package ru.skypro.homework.dto;
+
+
+
+public class InfoDTO {
+
+    private Long infoId;
+    private Long authorId;
+    private String userName;
+    private String authorFirstName;
+    private String authorLastName;
+    private String description;
+    private String email;
+    private String image;
+    private String phone;
+    private Double price;
+    private String title;
+
+}
