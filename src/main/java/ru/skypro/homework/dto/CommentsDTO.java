cb1e64ef68f6183462a6702eb6072647b587feae
HellHorseman <duke.sheffer@yandex.ru>
2023-12-09 19:07:52 +0400

    add properties/fix DTO to lombok library

--- src/main/java/ru/skypro/homework/dto/CommentsDTO.java
+++ src/main/java/ru/skypro/homework/dto/CommentsDTO.java
@@ -1,6 +1,8 @@
 package ru.skypro.homework.dto;
 
+import lombok.Data;
 
+@Data
 public class CommentsDTO {
 
     private Long id;
@@ -10,51 +12,4 @@ public class CommentsDTO {
     private Long commentId;
     private String text;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAuthorImage() {
-        return authorImage;
-    }
-
-    public void setAuthorImage(String authorImage) {
-        this.authorImage = authorImage;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public int getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(int createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Long getCommentId() {
-        return commentId;
-    }
-
-    public void setCommentId(Long commentId) {
-        this.commentId = commentId;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
--- src/main/java/ru/skypro/homework/dto/UserDto.java
+++ src/main/java/ru/skypro/homework/dto/UserDto.java
@@ -1,74 +1,17 @@
 package ru.skypro.homework.dto;
 
+
+import lombok.Data;
+
+@Data
 public class UserDto {
 
     private Long id;
-
     private String email;
-
     private String firstName;
-
     private String lastName;
-
     private String phone;
-
     private Role role;
-
     private String image;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 }
