e1acdde377fa5a402837fe95aa40574935135d58
ZaretskiyM <zaretwork@gmail.com>
2023-12-10 15:30:10 +0300

    Add GTO and Controller Ads

--- src/main/java/ru/skypro/homework/dto/Advertisement.java
+++ src/main/java/ru/skypro/homework/dto/Advertisement.java
@@ -3,17 +3,11 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 /**
  * @author Michail Z. (GH: HeimTN)
  */
-@Entity
 @Data
 public class Advertisement {
-    @Id
-    private long id;
     private long author;
     private String image;
     private int pk;
