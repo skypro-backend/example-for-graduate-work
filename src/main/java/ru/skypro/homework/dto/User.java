6319f716ca719deab9f3db805ad0f4e46464a688
ZaretskiyM <zaretwork@gmail.com>
2023-12-12 21:38:44 +0300

    Fix User: email add

--- src/main/java/ru/skypro/homework/dto/User.java
+++ src/main/java/ru/skypro/homework/dto/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class User {
     private int id;
+    private String email;
     private String firstName;
     private String lastName;
     private String phone;
