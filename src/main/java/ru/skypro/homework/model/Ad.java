98df3cfa435d8c25227bd12cb72e04ef78222189
SyutinS <syut@mail.ru>
2023-12-14 18:20:11 +0300

    созданы сущности и репозитории / без зависимостей

--- src/main/java/ru/skypro/homework/model/Ad.java
+++ src/main/java/ru/skypro/homework/model/Ad.java
@@ -1,12 +1,63 @@
 package ru.skypro.homework.model;
 
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
+
+/**
+ * The Ad class represents the essence of the ad in the database.
+ * It stores information about the ad,
+ * including its unique identifier (id), the id of the author of the ad, the link to the image of the ad,
+ * the ad id, the price of the ad and the title.
+ */
+
+@Entity
 @Data
+@NoArgsConstructor
+@Table(name = "ads")
 public class Ad {
-    private Integer author;
+
+    /**
+     * ID объявления
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ad_id")
+    private int pk;
+
+    /**
+     * Внешний ключ: ID автора из таблицы 'users'
+     * @see User
+     */
+//    @ManyToOne
+    @Column(name = "author_id")
+    private int author;
+
+    /**
+     * Внешний ключ: ссылка на фото из таблицы 'Avatar'
+     * @see Avatar
+     */
+//    @OneToOne
+    @Column(name = "image_id")
     private String image;
-    private Integer pk;
-    private Integer price;
+
+    /**
+     * Цена объявления
+     */
+    @Column(name = "price")
+    private int price;
+
+    /**
+     * Заголовок объявления
+     */
+    @Column(name = "title")
     private String title;
+
+    /**
+     * Внешний ключ: ID комментария из таблицы 'comments'
+     * @see Comment
+     */
+//    @OneToMany
+    @Column(name = "comment_id")
+    private int comment;
 }
--- src/main/java/ru/skypro/homework/model/Comment.java
+++ src/main/java/ru/skypro/homework/model/Comment.java
@@ -1,4 +1,55 @@
 package ru.skypro.homework.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "comments")
 public class Comment {
+
+    /**
+     * ID комментария
+     */
+    @Id
+//    @GeneratedValue
+    @Column(name = "comment_id")
+    private int pk;
+
+    /**
+     * Внешний ключ: ID автора из таблицы 'users'
+     * @see User
+     */
+//    @ManyToOne
+    @Column(name = "author_id")
+    private int author;
+
+    /**
+     * Внешний ключ: ссылка на аватар автора из таблицы 'Avatar'
+     * @see Avatar
+     */
+//    @OneToOne
+    @Column(name = "image_str")
+    private String authorImage;
+
+    /**
+     * Имя автора комментария
+     */
+    @Column(name = "author_name")
+    private String authorFirstName;
+
+    /**
+     * Дата и время создания комментария в миллисекундах с 00:00:00 01.01.1970
+     */
+    @Column(name = "created_at")
+    private Long createdAt;
+
+    /**
+     * Текст комментария
+     */
+    @Column(name = "comment_text")
+    private String text;
 }
