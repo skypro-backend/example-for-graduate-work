528d3cbd8ffd5fdd26c2c698058f3aedd5a398c0
vadim <vadimpoldolin19@gmail.com>
2023-12-17 16:41:11 +0100

    Добавил анн для updateUser

--- src/main/java/ru/skypro/homework/controller/UpdateUserController.java
+++ src/main/java/ru/skypro/homework/controller/UpdateUserController.java
@@ -1,12 +1,14 @@
 package ru.skypro.homework.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.service.UserService;
-
+@Slf4j
 @RestController
 @RequestMapping("http://localhost:8080/updateUser")
 public class UpdateUserController {
@@ -15,8 +17,8 @@ public class UpdateUserController {
     public UpdateUserController(UserService userService) {
         this.userService = userService;
     }
-
-    public ResponseEntity<UpdateUser> setInformationUser(@RequestBody UpdateUser updateUser){
-        return ResponseEntity.ok(userService.setInfoUser(updateUser));
-    }
+@PostMapping("/set_info_user")
+    public ResponseEntity<UpdateUser> setInformationUser(@RequestBody UpdateUser updateUser) {
+    return ResponseEntity.ok(userService.setInfoUser(updateUser));
+}
 }
