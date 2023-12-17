40e52c321a39c88508901031c822d441e17e68e9
SyutinS <syut@mail.ru>
2023-12-17 14:19:16 +0300

    зависимости + небольшие изменения классов

--- src/main/java/ru/skypro/homework/model/Ad.java
+++ src/main/java/ru/skypro/homework/model/Ad.java
@@ -3,6 +3,8 @@ package ru.skypro.homework.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The Ad class represents the essence of the ad in the database.
@@ -29,17 +31,17 @@ public class Ad {
      * Внешний ключ: ID автора из таблицы 'users'
      * @see User
      */
-//    @ManyToOne
-    @Column(name = "author_id")
-    private int author;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
 
     /**
-     * Внешний ключ: ссылка на фото из таблицы 'Avatar'
-     * @see Avatar
+     * Внешний ключ: ссылка на фото из таблицы 'images'
+     * @see Image
      */
     @OneToOne
     @JoinColumn(name = "image_id")
-    private Avatar image;
+    private Image image;
 
     /**
      * Цена объявления
@@ -54,10 +56,21 @@ public class Ad {
     private String title;
 
     /**
-     * Внешний ключ: ID комментария из таблицы 'comments'
+     * Внешний ключ: комментарии к объявлению из таблицы 'comments'
      * @see Comment
      */
 //    @OneToMany
-    @Column(name = "comment_id")
-    private int comment;
+//    @Column(name = "comment_id")
+//    private int comment;
+    @OneToMany(mappedBy = "ad", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+//    public Ad(int pk, User author, String title, String description, Image image, int price) {
+//        this.pk = pk;
+//        this.author = author;
+//        this.title = title;
+////        this.description = description;
+//        this.image = image;
+//        this.price = price;
+//    }
 }
