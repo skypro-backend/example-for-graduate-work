c63a9063f7198a90c14e0a7e8c437b53c1b891ac
Antonov333 <antonovsergei333@yahoo.com>
2023-12-19 20:59:21 +0300

    Ride through commit

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -19,9 +19,6 @@ public class AuthController {
 
     private final AuthService authService;
 
-    /*public AuthController(AuthService authService) {
-        this.authService = authService;
-    }*/
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Login login) {
