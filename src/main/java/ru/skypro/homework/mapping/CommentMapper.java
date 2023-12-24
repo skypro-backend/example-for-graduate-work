221efa9626389913186a50d31f9d80c45207347c
vadim <vadimpoldolin19@gmail.com>
2023-12-24 19:51:24 +0100

    AdMapper, CommentMapper and LoginMapper rework1.0

--- src/main/java/ru/skypro/homework/mapping/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapping/CommentMapper.java
@@ -3,9 +3,10 @@ package ru.skypro.homework.mapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import ru.skypro.homework.dto.CommentDto;
+import ru.skypro.homework.model.Comment;
 
 @Mapper
 public interface CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
-    CommentDto commentToDto(CommentDto commentDto);
+    CommentDto commentToDto(Comment comment);
 }
--- src/main/java/ru/skypro/homework/mapping/LoginMapper.java
+++ src/main/java/ru/skypro/homework/mapping/LoginMapper.java
@@ -5,9 +5,10 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import ru.skypro.homework.dto.CommentDto;
 import ru.skypro.homework.dto.LoginDto;
+import ru.skypro.homework.model.Login;
 
 @Mapper
 public interface LoginMapper {
     LoginMapper INSTANCE = Mappers.getMapper(LoginMapper.class);
-    LoginDto loginToDto(LoginDto loginDto);
+    LoginDto loginToDto(Login login);
 }
