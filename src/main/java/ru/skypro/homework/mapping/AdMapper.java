a080c3d3e7b9136a946e55424e26c666ca56a3dd
Antonov333 <antonovsergei333@yahoo.com>
2023-12-24 20:00:18 +0300

    Deleted unuseful classes and their mentions

--- src/main/java/ru/skypro/homework/mapping/AdMapper.java
+++ src/main/java/ru/skypro/homework/mapping/AdMapper.java
@@ -2,10 +2,9 @@ package ru.skypro.homework.mapping;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import ru.skypro.homework.dto.AdDto;
 
 @Mapper
 public interface AdMapper {
     AdMapper INSTANCE = Mappers.getMapper(AdMapper.class);
-    AdEntity adToAdEntity(AdDto ad);
+//    AdEntity adToAdEntity(AdDto ad);
 }
