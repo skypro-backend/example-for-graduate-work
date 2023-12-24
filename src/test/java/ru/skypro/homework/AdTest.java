52a551660fb237428bc3d93b0b3b691ff72400c6
Antonov333 <antonovsergei333@yahoo.com>
2023-12-24 20:41:12 +0300

    Unseful mapper deleted, temp code mentioned it removed

--- src/test/java/ru/skypro/homework/AdTest.java
+++ src/test/java/ru/skypro/homework/AdTest.java
@@ -1,18 +1,11 @@
 package ru.skypro.homework;
 
-import lombok.Data;
 import org.junit.jupiter.api.Test;
-import ru.skypro.homework.dto.AdDto;
-import ru.skypro.homework.entity.AdEntity;
-import ru.skypro.homework.entity.AdMapper;
 
 public class AdTest {
 
     @Test
    public void test1() {
-        AdDto ad = new AdDto();
-        ad.setAuthor(1111);
-        AdEntity adEntity = AdMapper.INSTANCE.adToAdEntity(ad);
-        System.out.println("adEntity.getAuthor() = " + adEntity.getAuthor());
-   }
+
+    }
 }
