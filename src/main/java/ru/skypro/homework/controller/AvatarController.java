67738ece0ca4c1f86307e8074ee54093c31313a1
Antonov333 <antonovsergei333@yahoo.com>
2023-12-24 17:07:16 +0300

    Created entity Images to keep images used in the application (user avatars and pictures of items in advertisements) and required SQL scripts, change annotation for multipartfile arg in the endpoint in AvatarController

--- src/main/java/ru/skypro/homework/controller/AvatarController.java
+++ src/main/java/ru/skypro/homework/controller/AvatarController.java
@@ -5,10 +5,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import ru.skypro.homework.service.UserService;
 
 import java.io.IOException;
@@ -22,7 +21,7 @@ public class AvatarController {
         this.userService = userService;
     }
     @PostMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> changeAvatar(@RequestParam MultipartFile avatar) throws IOException {
+    public ResponseEntity<?> changeAvatar(@RequestBody MultipartFile avatar) throws IOException {
         userService.setAvatar(avatar);
         return ResponseEntity.ok().build();
     }
