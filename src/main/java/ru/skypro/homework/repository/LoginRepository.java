898fd492563b7d9fbd48511c254c032942d71b8c
Antonov333 <antonovsergei333@yahoo.com>
2023-12-26 13:11:58 +0300

    Login entity deleted

--- src/main/java/ru/skypro/homework/repository/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.model.Login;
-
-public interface LoginRepository extends JpaRepository<Login, Long> {
-}
