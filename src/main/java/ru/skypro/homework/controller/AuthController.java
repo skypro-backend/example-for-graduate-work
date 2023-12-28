27c5c5f356d41a9bf66ba2028adb6a0757df84a8
Antonov333 <antonovsergei333@yahoo.com>
2023-12-28 16:49:38 +0300

    Created findByUserName method in UserRepository

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.homework.dto.LoginDto;
-
 import ru.skypro.homework.service.AuthService;
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -18,7 +17,6 @@ import ru.skypro.homework.service.AuthService;
 public class AuthController {
 
     private  AuthService authService;
-
 
     //public AuthController(AuthService authService) {
     //    this.authService = authService;
--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -4,6 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    /**
+     * <h2>Find user by user name in database</h2>
+     *
+     * @param userName arg to search user entity
+     * @return Optional
+     */
+    Optional<User> findByUserName(String userName);
 }
