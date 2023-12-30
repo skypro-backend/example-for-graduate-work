c846f9f1d91d3e22a070950b7830c774a0feadfd
HellHorseman <duke.sheffer@yandex.ru>
2023-12-30 16:23:40 +0400

    devide reg&auht service&controller. Create ConfigMapper, rename App

---
+++ src/main/java/ru/skypro/homework/KakAvitoApplication.java
@@ -0,0 +1,12 @@
+package ru.skypro.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakAvitoApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(KakAvitoApplication.class, args);
+  }
+
+}
---
+++ src/main/java/ru/skypro/homework/exceptions/AdNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.skypro.homework.exceptions;
+
+public class AdNotFoundException extends RuntimeException{
+    public AdNotFoundException(String message) {
+        super(message);
+    }
+}
