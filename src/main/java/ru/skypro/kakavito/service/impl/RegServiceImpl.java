251da23b24a33bc47b6e3d8821f7cf57ee4f2703
HellHorseman <duke.sheffer@yandex.ru>
2024-01-01 17:48:07 +0400

    update RegServiceImpl

--- src/main/java/ru/skypro/kakavito/service/impl/RegServiceImpl.java
+++ src/main/java/ru/skypro/kakavito/service/impl/RegServiceImpl.java
@@ -1,36 +1,53 @@
 package ru.skypro.kakavito.service.impl;
 
-import org.springframework.security.core.userdetails.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 import ru.skypro.kakavito.dto.Register;
+import ru.skypro.kakavito.model.User;
+import ru.skypro.kakavito.repository.UserRepo;
 import ru.skypro.kakavito.service.RegService;
 
+import java.util.regex.Pattern;
+
+
 @Service
+@RequiredArgsConstructor
 public class RegServiceImpl implements RegService {
 
-    private final UserDetailsManager manager;
+    private static final String PHONE_NUMBER_PATTERN = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}";;
     private final PasswordEncoder encoder;
+    private final UserRepo userRepo;
 
-    public RegServiceImpl(UserDetailsManager manager,
-                           PasswordEncoder passwordEncoder) {
-        this.manager = manager;
-        this.encoder = passwordEncoder;
-    }
 
     @Override
     public boolean register(Register register) {
-        if (manager.userExists(register.getUsername())) {
+        User foundUser = userRepo.findByEmail(register.getUsername()).orElse(null);
+        if (foundUser != null || !validateRegister(register)) {
             return false;
         }
-        manager.createUser(
-                User.builder()
-                        .passwordEncoder(this.encoder::encode)
-                        .password(register.getPassword())
-                        .username(register.getUsername())
-                        .roles(register.getRole().name())
-                        .build());
+
+        String password = encoder.encode(register.getPassword());
+
+        User newUser = new User();
+        newUser.setEmail(register.getUsername());
+        newUser.setPassword(password);
+        newUser.setFirstName(register.getFirstName());
+        newUser.setLastName(register.getLastName());
+        newUser.setPhone(register.getPhone());
+        newUser.setRole(register.getRole());
+        userRepo.save(newUser);
         return true;
+    }
+
+    private boolean validateRegister(Register register) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
+        boolean matcher = pattern.matcher(register.getPhone()).matches();
+
+        return (register.getUsername().length() >= 4 && register.getUsername().length() <= 32)
+                && (register.getPassword().length() >= 8 && register.getPassword().length() <= 16)
+                && (register.getFirstName().length() >= 2 && register.getFirstName().length() <= 16)
+                && (register.getLastName().length() >= 2 && register.getLastName().length() <= 16)
+                && (matcher);
     }
 }
