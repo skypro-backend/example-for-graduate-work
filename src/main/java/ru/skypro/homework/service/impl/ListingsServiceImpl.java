b4885593dc108b2fcb4036f18a58f74f8737bc9f
Mishator <voyteh89@mail.ru>
2024-01-03 17:31:57 +0300

    Создал все сервисы.

---
+++ src/main/java/ru/skypro/homework/service/impl/ListingsServiceImpl.java
@@ -0,0 +1,8 @@
+package ru.skypro.homework.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.service.ListingsService;
+
+@Service
+public class ListingsServiceImpl implements ListingsService {
+}
