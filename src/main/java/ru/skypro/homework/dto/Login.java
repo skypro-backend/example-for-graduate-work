58fe710b657844b9d81de54a11bdfa0e3b173cac
NARUTO9556 <gruver5263@gmail.com>
2024-02-09 20:49:23 +0800

    правки

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -4,12 +4,6 @@ import lombok.Data;
 
 @Data
 public class Login {
-    /**
-     * Имя пользователя
-     */
     private String username;
-    /**
-     * Пароль пользователя
-     */
     private String password;
 }
