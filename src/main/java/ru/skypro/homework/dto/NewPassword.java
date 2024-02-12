2641234e4f81d84e22b184b23ef74d943048643d
qmargo23 <elen.asplund@gmail.com>
2024-02-12 14:03:55 +0300

    8_NewPassword добавлены описания для полей согласующиеся со спецификацией OpenAPI

--- src/main/java/ru/skypro/homework/dto/NewPassword.java
+++ src/main/java/ru/skypro/homework/dto/NewPassword.java
@@ -1,6 +1,9 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import javax.validation.constraints.Size;
 
 /**
  * сущность NewPassword
@@ -10,9 +13,14 @@ public class NewPassword {
     /**
      * текущий пароль
      */
+    @Size(min = 8, max = 16)
+    @Schema(description = "текущий пароль")
     String currentPassword;
+
     /**
      * новый пароль
      */
+    @Size(min = 8, max = 16)
+    @Schema(description = "новый пароль")
     String newPassword;
 }
