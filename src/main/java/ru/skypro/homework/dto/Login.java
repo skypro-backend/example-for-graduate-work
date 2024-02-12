f8a4b5cad27216fa31550e4db572c29f8dcd25b5
Fet2113 <super.gjoll@yandex.ru>
2024-02-12 15:53:17 +0300

    добавил конструкторы

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -2,9 +2,45 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Login {
 
     private String username;
     private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Login)) return false;
+        Login login = (Login) o;
+        return Objects.equals(getUsername(), login.getUsername()) && Objects.equals(getPassword(), login.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/ru/skypro/homework/dto/Register.java
+++ src/main/java/ru/skypro/homework/dto/Register.java
@@ -2,6 +2,8 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Register {
 
@@ -11,4 +13,86 @@ public class Register {
     private String lastName;
     private String phone;
     private Role role;
+
+    public Register(String username, String password, String firstName, String lastName, String phone, Role role) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Register)) return false;
+        Register register = (Register) o;
+        return Objects.equals(getUsername(), register.getUsername()) && Objects.equals(getPassword(), register.getPassword()) && Objects.equals(getFirstName(), register.getFirstName()) && Objects.equals(getLastName(), register.getLastName()) && Objects.equals(getPhone(), register.getPhone()) && getRole() == register.getRole();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getPassword(), getFirstName(), getLastName(), getPhone(), getRole());
+    }
+
+    @Override
+    public String toString() {
+        return "Register{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
