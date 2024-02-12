a6a686be2c4269fc96b351e1ef6e2b62fd408bda
LuckyMirik <Luckymirik@mail.ru>
2024-02-12 12:20:01 +0300

    Немного JavaDoc

--- src/main/java/ru/skypro/homework/dto/Ad.java
+++ src/main/java/ru/skypro/homework/dto/Ad.java
@@ -1,12 +1,17 @@
 package ru.skypro.homework.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.Data;
 
+/**
+ * Сущность Ad
+ */
 @Data
 public class Ad {
     /**
      * ID автора объявления
      */
+    @Schema(description = "id автора объявления")
     private int author;
     /**
      * Ссылка на картинку объявления
--- src/main/java/ru/skypro/homework/dto/Ads.java
+++ src/main/java/ru/skypro/homework/dto/Ads.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Сущность Ads
+ */
 @Data
 public class Ads {
     /**
