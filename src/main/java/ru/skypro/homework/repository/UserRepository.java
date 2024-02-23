374545736054f86993c792ff926499f568a0b3d0
Mishator <voyteh89@mail.ru>
2024-02-23 15:00:30 +0300

    Реализован UserRepository, AvatarService и его имплемента́ция.

--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.User;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmailIgnoreCase(String email);
+
+    boolean existUserByEmailIgnoreCase(String email);
 
 }
--- src/main/java/ru/skypro/homework/service/AvatarService.java
+++ src/main/java/ru/skypro/homework/service/AvatarService.java
@@ -1,4 +1,15 @@
 package ru.skypro.homework.service;
 
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.entity.Avatar;
+
+import java.io.IOException;
+
 public interface AvatarService {
+
+    Avatar uploadAvatar(MultipartFile image) throws IOException;
+
+    void removeAvatar(Avatar avatar);
+
+    Avatar getAvatar(Long id);
 }
