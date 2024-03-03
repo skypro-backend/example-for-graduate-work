a259747c58b411e3e51e33ce4ca202c2fed5c5ce
YuZu <yurkazubkov@yandex.ru>
2024-03-03 13:32:21 +0300

    added doc for AdService

--- src/main/java/ru/skypro/homework/service/AdService.java
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -7,18 +7,63 @@ import ru.skypro.homework.model.Ad;
 import java.util.List;
 
 
+/**
+ * Service for working with ads.
+ */
 public interface AdService {
+    /**
+     * Returns all ads.
+     *
+     * @return all ads
+     */
     List<Ad> getAllAds();
 
+    /**
+     * Returns the ad by id.
+     *
+     * @param id ad id
+     * @return the ad
+     */
     Ad getAdById(Long id);
 
+    /**
+     * Creates a new ad.
+     *
+     * @param dto       information about new ad
+     * @param imageFile image file
+     * @return created ad
+     */
     Ad createAd(CreateOrUpdateAd dto, MultipartFile imageFile);
 
+    /**
+     * Updates the ad by id.
+     *
+     * @param id  ad id
+     * @param dto new information
+     * @return updated ad
+     */
     Ad updateAd(Long id, CreateOrUpdateAd dto);
 
+    /**
+     * Deletes the ad by id.
+     *
+     * @param id ad id
+     */
     void deleteAd(Long id);
 
+    /**
+     * Returns the ads of the current user.
+     *
+     * @return the ads of the current user
+     */
     List<Ad> getMyAds();
 
+    /**
+     * Updates ad image by id.
+     *
+     * @param id        ad id
+     * @param imageFile new image file
+     * @return updated ad
+     */
     Ad updateAdImage(Long id, MultipartFile imageFile);
 }
