3405681a4c6d8f06099a6bef57d06b8889fcc021
YuZu <yurkazubkov@yandex.ru>
2024-03-03 13:48:45 +0300

    added doc for ImageService

--- src/main/java/ru/skypro/homework/service/ImageService.java
+++ src/main/java/ru/skypro/homework/service/ImageService.java
@@ -3,8 +3,23 @@ package ru.skypro.homework.service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.model.Image;
 
+/**
+ * Service for working with images.
+ */
 public interface ImageService {
+    /**
+     * Saves the image.
+     *
+     * @param imageFile image file
+     * @return saved image
+     */
     Image saveImage(MultipartFile imageFile);
 
+    /**
+     * Returns the image by id.
+     *
+     * @param id image id
+     * @return the image by id
+     */
     Image getImageById(Long id);
 }
