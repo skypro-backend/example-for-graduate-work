4f20f994c9e7d30764f3d1253ce8cbefa5064df2
YuZu <yurkazubkov@yandex.ru>
2024-03-03 13:39:10 +0300

    added doc for AuthService

--- src/main/java/ru/skypro/homework/service/AuthService.java
+++ src/main/java/ru/skypro/homework/service/AuthService.java
@@ -3,8 +3,23 @@ package ru.skypro.homework.service;
 import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.model.User;
 
+/**
+ * Service for working with auth.
+ */
 public interface AuthService {
+    /**
+     * Authorizes the user by username and password.
+     *
+     * @param userName username
+     * @param password password
+     */
     void login(String userName, String password);
 
+    /**
+     * Registers the user.
+     *
+     * @param register info about user
+     * @return created user
+     */
     User register(Register register);
 }
