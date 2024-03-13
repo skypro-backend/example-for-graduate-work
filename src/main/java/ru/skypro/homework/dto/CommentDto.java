95b413569bab80bf3763c3b038dbd0ba134e2b49
el-kg <131970333+el-kg@users.noreply.github.com>
2024-03-13 20:46:45 +0700

    написал CommentService его имплементацию(пока пустую),CommentRepository

---
+++ src/main/java/ru/skypro/homework/dto/CommentDto.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework.dto;
+import lombok.Data;
+@Data
+public class CommentDto {
+        private Integer author;
+        private String authorImage;
+        private String authorFirstName;
+        private Long createdAt;
+        private Integer pk;
+        private String text;
+}
--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -1,4 +1,44 @@
 package ru.skypro.homework.service;
 
+import ru.skypro.homework.dto.CommentDto;
+import org.springframework.security.core.Authentication;
+
 public interface CommentService {
+    /**
+     * Метод для получения всех комментарий к объявлению
+     *
+     * @param id идентификатор объявления
+     * @return возвращает комментарии
+     */
+    CommentDto getComments(int id);
+
+    /**
+     * Метод для добавления комментария в объявление
+     *
+     * @param id   идентификатор объявления
+     * @param text текст комментария
+     * @return возвращает добавленный комментарий
+     */
+    CommentDto addComment(int id, CommentDto text);
+
+    /**
+     * Удаляет комментарий
+     *
+     * @param adId           идентификатор объявления
+     * @param commentId      идентификатор комментария
+     * @param authentication объект {@link Authentication}
+     * @return <code>true</code> если комментарий удален, <code>false</code> в случае неудачи
+     */
+    boolean deleteComment(int adId, int commentId, Authentication authentication);
+
+    /**
+     * Обновляет комментарий
+     *
+     * @param adId           идентификатор объявления
+     * @param commentId      идентификатор комментария
+     * @param commentDto     объект {@link CommentDto}
+     * @param authentication объект {@link Authentication}
+     * @return возвращает обновленный комментарий
+     */
+    CommentDto updateComment(Integer adId, Integer commentId, CommentDto commentDto, Authentication authentication);
 }
